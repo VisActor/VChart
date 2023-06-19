347f2c3a8d480f14c86faa57a12286ef3b646b8b
Howard Zhang <zamhown@live.com>
2023-06-19 15:41:26 +0800

    fix: boxplot shaft stroke

--- packages/vchart/src/theme/buildin-theme/light/series/box-plot.ts
+++ packages/vchart/src/theme/buildin-theme/light/series/box-plot.ts
@@ -3,7 +3,6 @@ import type { IBoxPlotSeriesTheme } from '../../../../series/box-plot/interface'
 export const boxPlot: IBoxPlotSeriesTheme = {
   boxPlot: {
     style: {
-      stroke: '#000000',
       lineWidth: 1
     }
   }
