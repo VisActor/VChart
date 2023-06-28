05114bc3764fe74752c5588bd432ed7dbd423287
kkxxkk2019 <just_star@qq.com>
2023-06-28 10:46:48 +0800

    chore: remove compact code

--- packages/vchart/src/mark/polygon.ts
+++ packages/vchart/src/mark/polygon.ts
@@ -17,42 +17,6 @@ class BasePolygonMark<T extends ICommonSpec> extends BaseMark<T> {
     };
     return defaultStyle;
   }
-
-  /**
-   * @override 兼容下 polygon 的 borderRadius
-   * 由外部series调用，设置markStyle的接口（如果由 IModel 派生类调用，请使用 IModel.setMarkStyle）
-   * @param style
-   * @param level
-   * @param state
-   */
-  setStyle<U extends keyof T>(
-    style: Partial<IMarkStyle<T>>,
-    state: StateValueType = 'normal',
-    level: number = 0,
-    stateStyle = this.stateStyle
-  ): void {
-    if (isNil(style)) {
-      return;
-    }
-    style = this._filterStyle(style, state, level, stateStyle);
-
-    if (stateStyle[state] === undefined) {
-      stateStyle[state] = {};
-    }
-
-    const isUserLevel = this.isUserLevel(level);
-
-    Object.keys(style).forEach((attr: string) => {
-      let attrStyle = style[attr] as MarkInputStyle<T[U]>;
-      if (isNil(attrStyle)) {
-        return;
-      }
-
-      attrStyle = this._filterAttribute(attr as any, attrStyle, state, level, isUserLevel, stateStyle);
-
-      this.setAttribute((attr === 'borderRadius' ? 'cornerRadius' : attr) as any, attrStyle, state, level, stateStyle);
-    });
-  }
 }
 
 export class PolygonMark extends BasePolygonMark<IPolygonMarkSpec> implements IPolygonMark {
