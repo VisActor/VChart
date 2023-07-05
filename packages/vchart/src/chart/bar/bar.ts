707f6733536358c8d403cdfb694143246a29b23b
lixuefei.1313 <lixuef1313@163.com>
2023-07-05 19:51:12 +0800

    fix: fix barWidth not work in bar chart

--- packages/vchart/src/chart/bar/bar.ts
+++ packages/vchart/src/chart/bar/bar.ts
@@ -4,6 +4,7 @@ import { ChartTypeEnum } from '../interface';
 import { setDefaultCrosshairForCartesianChart } from '../util';
 import { VChart } from '../../core/vchart';
 import { BarSeries } from '../../series';
+import type { IBarChartSpec } from './interface';
 VChart.useSeries([BarSeries]);
 
 export class BarChart extends CartesianChart {
@@ -12,8 +13,17 @@ export class BarChart extends CartesianChart {
   readonly type: string = ChartTypeEnum.bar;
   readonly seriesType: string = SeriesTypeEnum.bar;
 
-  transformSpec(spec: any): void {
+  transformSpec(spec: IBarChartSpec): void {
     super.transformSpec(spec);
     setDefaultCrosshairForCartesianChart(spec);
+  }
+
+  protected _getDefaultSeriesSpec(spec: IBarChartSpec): any {
+    return {
+      ...super._getDefaultSeriesSpec(spec),
+      barWidth: spec.barWidth,
+      barMinWidth: spec.barMinWidth,
+      barMaxWidth: spec.barMaxWidth
+    };
   }
 }
