54f82724a7f3d13a657086a852bf17869bf683c1
kkxxkk2019 <just_star@qq.com>
2023-07-05 18:19:49 +0800

    chore: add copy task for vchart after build

--- packages/vchart/bundler.config.js
+++ packages/vchart/bundler.config.js
@@ -4,6 +4,21 @@
 const fs = require('fs');
 const path = require('path');
 
+function copyFile(source, target) {
+  fs.copyFile(source, target, error => {
+    if (error) {
+      console.error(err);
+    }
+    console.log('File copied successfully!');
+  });
+}
+
+function copyStart(source, destinations) {
+  destinations.forEach(target => {
+    copyFile(source, target);
+  });
+}
+
 module.exports = {
   name: 'VChart',
   formats: ['es', 'cjs', 'umd'],
@@ -32,6 +47,15 @@ module.exports = {
         const dtsCode = fs.readFileSync(path.join(__dirname, `../../common/template/${entryName}.d.ts`), 'utf-8');
         fs.writeFileSync(path.join(__dirname, `./${entryName}.d.ts`), dtsCode, 'utf-8');
       });
+    },
+    copy: (config, projectRoot, rawPackageJson) => {
+      const vchartSource = path.join(__dirname, config.outputDir.umd, 'index.min.js');
+      const packagesDestinations = [
+        path.join(__dirname, '../tt-vchart/src/vchart/index.js'),
+        path.join(__dirname, '../lark-vchart/src/vchart/index.js')
+      ];
+
+      copyStart(vchartSource, packagesDestinations);
     }
   }
 };
