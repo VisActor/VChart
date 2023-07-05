97810b7803d75a19ac99984e18955861fff38201
kkxxkk2019 <just_star@qq.com>
2023-07-05 15:06:11 +0800

    chore: optimize the project configuration of @visactor/taro-vchart

--- packages/taro-vchart/bundler.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  formats: ['cjs', 'es', 'umd']
-};
--- packages/taro-vchart/src/index.tsx
+++ packages/taro-vchart/src/index.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { WebChart } from './components/web-chart';
 import { GeneralChart } from './components/general-chart';
 import { IChartProps, VChartEnvType } from './typings';
+import Taro from '@tarojs/taro';
 
 interface IVChartProps extends IChartProps {
   /**
-   * 配置环境。
+   * 配置环境。如果没有声明，则会通过 `Taro.getEnv()` 自动获取。
    * - `tt` 字节小程序。
    * - `lark` 飞书小程序。
    * - `h5` 浏览器环境, 与`web`等价。
    * - `web` 浏览器环境, 与`h5`等价。
    */
-  type: VChartEnvType;
+  type?: VChartEnvType;
 }
 
 export default function VChart({ type, ...args }: IVChartProps) {
-  const env = type.toLocaleLowerCase();
+  const env = type ? type.toLocaleLowerCase() : Taro.getEnv();
   const strategies = {
     lark: () => {
       return <GeneralChart {...args} />;
