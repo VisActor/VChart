fd96a6fc7936a48e22991f1384339a3ec6aed03c
zhouxinyu <zhouxinyu12138@gmail.com>
2023-07-21 11:05:11 +0800

    feat: merge exclude into optimizeDeps

--- packages/vchart/__tests__/runtime/browser/vite.config.js
+++ packages/vchart/__tests__/runtime/browser/vite.config.js
@@ -4,18 +4,6 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default {
-  optimizeDeps: {
-    exclude: [
-      '@visactor/vrender',
-      '@visactor/vgrammar',
-      '@visactor/vgrammar-projection',
-      '@visactor/vgrammar-hierarchical',
-      '@visactor/vgrammar-label',
-      '@visactor/vgrammar-wordcloud',
-      '@visactor/vgrammar-wordcloud-fast',
-      '@visactor/vscale'
-    ]
-  },
   server: {
     host: '0.0.0.0',
     port: localConf.port || 3000,
@@ -26,6 +14,16 @@ export default {
     __VERSION__: JSON.stringify(require('../../../package.json').version)
   },
   optimizeDeps: {
+    exclude: [
+      '@visactor/vrender',
+      '@visactor/vgrammar',
+      '@visactor/vgrammar-projection',
+      '@visactor/vgrammar-hierarchical',
+      '@visactor/vgrammar-label',
+      '@visactor/vgrammar-wordcloud',
+      '@visactor/vgrammar-wordcloud-fast',
+      '@visactor/vscale'
+    ],
     esbuildOptions: {
       // Node.js global to browser globalThis
       define: {
