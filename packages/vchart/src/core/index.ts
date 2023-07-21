74f520e46dba64262ab965ad99023777ac71b0ed
lixuefei.1313 <lixuef1313@163.com>
2023-07-21 17:13:18 +0800

    docs: update changlog of rush

--- packages/vchart/src/core/index.ts
+++ packages/vchart/src/core/index.ts
@@ -1,3 +1,4 @@
+import { Logger, LoggerLevel } from '@visactor/vutils';
 /**
  * @description The core module of VChart, containing the necessary interfaces for using VChart.
  */
@@ -30,3 +31,6 @@ export * from '../event/interface';
 export * from '../typings/spec/common';
 export type { IStateSpec, StateValueType } from '../compile/mark';
 export * from '../theme/interface';
+
+// set default logger level to Level.error
+Logger.getInstance(LoggerLevel.Error);
