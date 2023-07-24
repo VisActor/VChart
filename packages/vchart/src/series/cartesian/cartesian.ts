9d090cbed3fb4e76d570762b68eecaf6f219fa67
kkxxkk2019 <just_star@qq.com>
2023-07-24 10:54:43 +0800

    fix(vchart): fix the issue that fieldX2 does not involved in statistics. closed#254

--- packages/vchart/src/series/cartesian/cartesian.ts
+++ packages/vchart/src/series/cartesian/cartesian.ts
@@ -127,7 +127,7 @@ export abstract class CartesianSeries<T extends ICartesianSeriesSpec = ICartesia
   getStatisticFields() {
     const fields: { key: string; operations: StatisticOperations }[] = [];
     if (this.getXAxisHelper()?.getScale) {
-      this._fieldX.forEach(f => {
+      (this._fieldX2 ? [...this._fieldX, this._fieldX2] : this._fieldX).forEach(f => {
         const result: { key: string; operations: Array<'max' | 'min' | 'values'> } = { key: f, operations: [] };
         if (isContinuous(this.getXAxisHelper().getScale(0).type)) {
           result.operations = ['max', 'min'];
@@ -138,7 +138,7 @@ export abstract class CartesianSeries<T extends ICartesianSeriesSpec = ICartesia
       });
     }
     if (this.getYAxisHelper()?.getScale) {
-      this._fieldY.forEach(f => {
+      (this._fieldY2 ? [...this._fieldY, this._fieldY2] : this._fieldY).forEach(f => {
         const result: { key: string; operations: Array<'max' | 'min' | 'values'> } = { key: f, operations: [] };
         if (isContinuous(this.getYAxisHelper().getScale(0).type)) {
           result.operations = ['max', 'min'];
