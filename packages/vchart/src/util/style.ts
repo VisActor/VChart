f9e1da97e1673d306bf6ced72f813eae6c6eac07
kkxxkk2019 <just_star@qq.com>
2023-07-24 13:58:44 +0800

    feat(vchart): discrete legend's item's style and state style supports function

--- packages/vchart/src/util/style.ts
+++ packages/vchart/src/util/style.ts
@@ -1,5 +1,6 @@
 import { degreeToRadian, isEmpty, isFunction, isValid, isValidNumber } from '@visactor/vutils';
 import type { Datum } from '../typings';
+import type { LegendItemDatum } from '@visactor/vrender-components';
 
 /**
  * 针对一些可以配置状态样式的属性的转换函数，结构如下：
@@ -9,12 +10,20 @@ import type { Datum } from '../typings';
  */
 export function transformComponentStyle(cfg: any = {}) {
   if (!isEmpty(cfg.style)) {
+    if (isFunction(cfg.style)) {
+      cfg.style = (item: LegendItemDatum, isSelected: boolean, index: number, allItems: LegendItemDatum[]) =>
+        cfg.style(item, isSelected, index, allItems);
+    }
     cfg.style = transformToGraphic(cfg.style);
   }
 
   if (!isEmpty(cfg.state)) {
     Object.keys(cfg.state).forEach(key => {
       if (!isEmpty(cfg.state[key])) {
+        if (isFunction(cfg.state[key])) {
+          cfg.state[key] = (item: LegendItemDatum, isSelected: boolean, index: number, allItems: LegendItemDatum[]) =>
+            cfg.state[key](item, isSelected, index, allItems);
+        }
         cfg.state[key] = transformToGraphic(cfg.state[key]);
       }
     });
@@ -31,10 +40,9 @@ export function transformStateStyle(stateStyle: any) {
     if (!isEmpty(stateStyle[key])) {
       if (isFunction(stateStyle[key])) {
         stateStyle[key] = (value: any, index: number, datum: Datum, data: Datum[]) =>
-          transformToGraphic(stateStyle[key](value, index, datum, data));
-      } else {
-        stateStyle[key] = transformToGraphic(stateStyle[key]);
+          stateStyle[key](value, index, datum, data);
       }
+      stateStyle[key] = transformToGraphic(stateStyle[key]);
     }
   });
 
@@ -49,10 +57,9 @@ export function transformAxisLabelStateStyle(stateStyle: any) {
     if (!isEmpty(stateStyle[key])) {
       if (isFunction(stateStyle[key])) {
         stateStyle[key] = (datum: Datum, index: number, data: Datum[], layer?: number) =>
-          transformToGraphic(stateStyle[key](datum.rawValue, index, datum, data, layer));
-      } else {
-        stateStyle[key] = transformToGraphic(stateStyle[key]);
+          stateStyle[key](datum.rawValue, index, datum, data, layer);
       }
+      stateStyle[key] = transformToGraphic(stateStyle[key]);
     }
   });
 
