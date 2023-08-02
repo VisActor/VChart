e51a617d363e114e511cb030c7f2d3896c9d2df9
kkxxkk2019 <just_star@qq.com>
2023-08-02 22:59:40 +0800

    feat(vchart): support `barGapInGroup` for bar series and rangeColumn series, used to set the spacing between bars within a group, relate #328

--- packages/vchart/src/series/bar/interface.ts
+++ packages/vchart/src/series/bar/interface.ts
@@ -37,17 +37,30 @@ export interface IBarSeriesSpec
       | 'inside-left';
   };
   /**
-   * 柱体宽度
+   * 柱体宽度，可以设置绝对的像素值，也可以使用百分比（如 '10%'）
+   * 1. number 类型，表示像素值
+   * 2. string 类型，百分比用法，如 '10%'，该值为每个柱子的宽度（即每个分组的总宽度减去 barGapInGroup 后的宽度处于组内个数）的占比
    */
-  barWidth?: number;
+  barWidth?: number | string;
   /**
-   * 柱体最小宽度
+   * 柱体最小宽度，可以设置绝对的像素值，也可以使用百分比（如 '10%'）
+   * 1. number 类型，表示像素值
+   * 2. string 类型，百分比用法，如 '10%'，该值为每个柱子的宽度（即每个分组的总宽度减去 barGapInGroup 后的宽度处于组内个数）的占比
    */
-  barMinWidth?: number;
+  barMinWidth?: number | string;
   /**
-   * 柱体最大宽度
+   * 柱体最大宽度，可以设置绝对的像素值，也可以使用百分比（如 '10%'）
+   * 1. number 类型，表示像素值
+   * 2. string 类型，百分比用法，如 '10%'，该值为每个柱子的宽度（即每个分组的总宽度减去 barGapInGroup 后的宽度处于组内个数）的占比
    */
-  barMaxWidth?: number;
+  barMaxWidth?: number | string;
+  /**
+   * 分组柱图中各个分组内的柱子间距，可以设置绝对的像素值，也可以使用百分比（如 '10%'）
+   * 1. number 类型，表示像素值
+   * 2. string 类型，百分比用法，如 '10%'，该值为每个分组的总宽度占比
+   * @since 1.2.0
+   */
+  barGapInGroup?: number | string;
 }
 
 export interface IBarSeriesTheme extends ICartesianSeriesTheme {
