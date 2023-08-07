67c1981f46e7a7596f02ae0a2fdcf4f3b983a076
xiaoluoHe <sevnape@outlook.com>
2023-08-07 19:44:34 +0800

    chore: fix test error

--- packages/vchart/src/component/geo/geo-coordinate.ts
+++ packages/vchart/src/component/geo/geo-coordinate.ts
@@ -209,7 +209,8 @@ export class GeoCoordinate extends BaseComponent implements IGeoCoordinate {
               return this.dataToLongitude(values[0]);
             },
             getAxisType: () => this.type,
-            getAxisId: () => this.id
+            getAxisId: () => this.id,
+            isInverse: () => false
           });
           (s as unknown as ICartesianSeries).setYAxisHelper({
             ...helper,
@@ -217,7 +218,8 @@ export class GeoCoordinate extends BaseComponent implements IGeoCoordinate {
               return this.dataToLatitude(values[0]);
             },
             getAxisType: () => this.type,
-            getAxisId: () => this.id
+            getAxisId: () => this.id,
+            isInverse: () => false
           });
           this._longitudeField && (s as unknown as ICartesianSeries).setFieldX(this._longitudeField);
           this._latitudeField && (s as unknown as ICartesianSeries).setFieldY(this._latitudeField);
