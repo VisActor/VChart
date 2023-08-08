5fc968d7df85fed3b0db338e80832f5bccbcc655
kkxxkk2019 <just_star@qq.com>
2023-08-08 21:28:33 +0800

    fix(vchart): fix the issue of axis label flush config disable when axis inverse is true, fixed #449

--- packages/vchart/src/plugin/components/axis-label-overlap-plugin.ts
+++ packages/vchart/src/plugin/components/axis-label-overlap-plugin.ts
@@ -10,45 +10,88 @@ import type { CartesianAxis } from '../../component/axis/cartesian';
 function labelFlush(axisLabels: IText[], axis: IComponent) {
   const { width, height } = axis.getLayoutRect();
   const isX = isXAxis(axis.layoutOrient);
+  const isInverse = axis.getSpec().inverse === true;
   const first = axisLabels[0];
   const last = peek(axisLabels);
 
   if (isX) {
-    const start = 0;
-    const end = width;
-    const startBound = first.AABBBounds.x1;
-    const endBound = last.AABBBounds.x2;
-    if (startBound < start) {
-      first.setAttributes({
-        x: start,
-        textAlign: 'left'
-      });
-    }
+    if (isInverse) {
+      const start = width;
+      const end = 0;
+      const startBound = first.AABBBounds.x2;
+      const endBound = last.AABBBounds.x1;
 
-    if (endBound > end) {
-      last.setAttributes({
-        x: end,
-        textAlign: 'right'
-      });
+      if (startBound > start) {
+        first.setAttributes({
+          x: start,
+          textAlign: 'right'
+        });
+      }
+
+      if (endBound < end) {
+        last.setAttributes({
+          x: end,
+          textAlign: 'left'
+        });
+      }
+    } else {
+      const start = 0;
+      const end = width;
+      const startBound = first.AABBBounds.x1;
+      const endBound = last.AABBBounds.x2;
+      if (startBound < start) {
+        first.setAttributes({
+          x: start,
+          textAlign: 'left'
+        });
+      }
+
+      if (endBound > end) {
+        last.setAttributes({
+          x: end,
+          textAlign: 'right'
+        });
+      }
     }
   } else {
-    const start = height;
-    const end = 0;
-    const startBound = first.AABBBounds.y2;
-    const endBound = last.AABBBounds.y1;
+    if (isInverse) {
+      const startBound = first.AABBBounds.y1;
+      const endBound = last.AABBBounds.y2;
+      const start = 0;
+      const end = height;
 
-    if (startBound > start) {
-      first.setAttributes({
-        y: start,
-        textBaseline: 'bottom'
-      });
-    }
+      if (startBound < start) {
+        first.setAttributes({
+          y: start,
+          textBaseline: 'top'
+        });
+      }
 
-    if (endBound < end) {
-      last.setAttributes({
-        y: end,
-        textBaseline: 'top'
-      });
+      if (endBound > end) {
+        last.setAttributes({
+          y: end,
+          textBaseline: 'bottom'
+        });
+      }
+    } else {
+      const start = height;
+      const end = 0;
+      const startBound = first.AABBBounds.y2;
+      const endBound = last.AABBBounds.y1;
+
+      if (startBound > start) {
+        first.setAttributes({
+          y: start,
+          textBaseline: 'bottom'
+        });
+      }
+
+      if (endBound < end) {
+        last.setAttributes({
+          y: end,
+          textBaseline: 'top'
+        });
+      }
     }
   }
 }
