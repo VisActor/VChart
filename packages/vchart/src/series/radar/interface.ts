cbabaaa31ded125524b8da805b620fc0497a30b9
lixuefei.1313 <lixuef1313@163.com>
2023-08-15 17:29:29 +0800

    feat: add configruation activePoint for enable show point when set point visible false

--- packages/vchart/src/series/radar/interface.ts
+++ packages/vchart/src/series/radar/interface.ts
@@ -33,6 +33,13 @@ export interface IRadarSeriesSpec
    * @since 1.2.0
    */
   seriesMark?: 'point' | 'line' | 'area';
+
+  /**
+   * 是否使用额外的 activePoint 显示交互点，可以在点隐藏时显示被交互的点
+   * @default false
+   * @since 1.3.0
+   */
+  activePoint?: boolean;
 }
 
 export interface IRadarSeriesTheme extends IRoseLikeSeriesTheme, ILineLikeSeriesTheme {
