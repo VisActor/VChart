2093a52c380c9369ad08ba30dd9ce15f7a0cb466
xiaoluoHe <sevnape@outlook.com>
2023-08-17 20:48:51 +0800

    feat: react-vchart disable exitAnimation when update

--- packages/react-vchart/src/charts/BaseChart.tsx
+++ packages/react-vchart/src/charts/BaseChart.tsx
@@ -139,7 +139,8 @@ const BaseChart: React.FC<Props> = React.forwardRef((props, ref) => {
         eventsBinded.current = props;
         // eslint-disable-next-line promise/catch-or-return
         const updatePromise = chartContext.current.chart
-          .updateSpec(parseSpec(props), undefined, { morph: false }) // morph临时关掉
+          //@ts-ignore
+          .updateSpec(parseSpec(props), undefined, { morph: false, enableExitAnimation: false }) // morph临时关掉
           .then(handleChartRender);
 
         if (props.onError) {
@@ -155,7 +156,8 @@ const BaseChart: React.FC<Props> = React.forwardRef((props, ref) => {
       prevSpec.current = newSpec;
       // eslint-disable-next-line promise/catch-or-return
       const updatePromise = chartContext.current.chart
-        .updateSpec(parseSpec(props), undefined, { morph: false }) // morph临时关掉
+        // @ts-ignore
+        .updateSpec(parseSpec(props), undefined, { morph: false, enableExitAnimation: false }) // morph临时关掉
         .then(handleChartRender);
 
       if (props.onError) {
