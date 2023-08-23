bd4a0a4e10700240906d2beff584502fcfd12627
lixuefei.1313 <lixuef1313@163.com>
2023-08-23 16:09:32 +0800

    feat: add area series tooltip helpter to get tooltip symbol style

---
+++ packages/vchart/src/series/area/tooltip-helpter.ts
@@ -0,0 +1,24 @@
+import type { ISeriesTooltipHelper } from '../interface';
+import { BaseSeriesTooltipHelper } from '../base/tooltip-helper';
+import { array, isValid } from '../../util';
+
+export class AreaSeriesTooltipHelper extends BaseSeriesTooltipHelper implements ISeriesTooltipHelper {
+  protected _getSeriesStyle = (datum: any, styleKey: string | string[], defaultValue?: any) => {
+    for (const key of array(styleKey)) {
+      let value = this.series.getSeriesStyle(datum)?.(key);
+      // because of line mark merge into area mark
+      // TODO: if tooltip symbol use mark fill & stroke ,remove this code
+      if (value === false && (key === 'fill' || key === 'stroke')) {
+        if (key === 'fill') {
+          value = this.series.getSeriesStyle(datum)?.('stroke')?.[0];
+        } else {
+          value = this.series.getSeriesStyle(datum)?.('fill');
+        }
+      }
+      if (isValid(value)) {
+        return value;
+      }
+    }
+    return defaultValue;
+  };
+}
