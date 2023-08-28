7d073721019fe5b9f7e7def0712658e3c8ae0f14
lixuefei.1313 <lixuef1313@163.com>
2023-08-28 15:03:15 +0800

    feat: support configuration of sortDataByAxis for sort series data with axis, details in #644

--- packages/vchart/src/series/util/utils.ts
+++ packages/vchart/src/series/util/utils.ts
@@ -4,6 +4,21 @@ import type { Datum } from '../../typings';
 import { isContinuous } from '@visactor/vscale';
 import type { IPolarAxisHelper } from '../../component/axis';
 
+/**
+ * 将相对数值转换为绝对数值
+ * @param originValue 原始值（相对值或绝对值）
+ * @param total 总体值
+ * @returns 实际绝对数值
+ */
+export const getActualNumValue = (originValue: number | string, total: number): number => {
+  const originNumValue = Number(originValue);
+  const originStrValue = originValue.toString();
+  if (isNaN(originNumValue) && originStrValue[originStrValue.length - 1] === '%') {
+    return total * (Number(originStrValue.slice(0, originStrValue.length - 1)) / 100);
+  }
+  return originNumValue;
+};
+
 export function isPolarAxisSeries(type: string) {
   return ([SeriesTypeEnum.rose, SeriesTypeEnum.radar, SeriesTypeEnum.circularProgress] as string[]).includes(type);
 }
