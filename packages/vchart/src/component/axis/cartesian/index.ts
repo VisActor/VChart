e45950526988c5dc114c76446ea6e00430353925
skie1997 <skie1997@outlook.com>
2023-08-29 11:01:49 +0800

    fix: export problem

--- packages/vchart/src/component/axis/cartesian/index.ts
+++ packages/vchart/src/component/axis/cartesian/index.ts
@@ -2,4 +2,6 @@ export * from './axis';
 export * from './linear-axis';
 export * from './band-axis';
 export * from './time-axis';
+export * from './log-axis';
+export * from './symlog-axis';
 export * from './interface';
