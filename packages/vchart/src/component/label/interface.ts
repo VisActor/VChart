4dc8183eb9e3e5b063de256d0e41ac0e3d29b04d
Howard Zhang <zamhown@live.com>
2023-08-30 19:53:02 +0800

    fix: spec type problem in label component

--- packages/vchart/src/component/label/interface.ts
+++ packages/vchart/src/component/label/interface.ts
@@ -1,7 +1,8 @@
 import type { BaseLabelAttrs } from '@visactor/vrender-components';
 import type { ConvertToMarkStyleSpec, ITextMarkSpec } from '../../typings';
+import type { IComponentSpec } from '../base/interface';
 
-export interface ILabelSpec {
+export interface ILabelSpec extends IComponentSpec {
   /** 默认不显示标签 */
   visible?: boolean;
   /** 是否支持交互。@default false */
