726996a2f71b29781cb1844c9371142be2c7fad5
kkxxkk2019 <just_star@qq.com>
2023-09-01 22:41:00 +0800

    fix: fix the issue of pie chart with null value, fixed https://github.com/VisActor/VChart/issues/748

--- packages/vchart/src/data/transforms/pie.ts
+++ packages/vchart/src/data/transforms/pie.ts
@@ -1,6 +1,6 @@
 import type { DataView } from '@visactor/vdataset';
 import type { Datum } from '../../typings';
-import { computeQuadrant } from '../../util';
+import { computeQuadrant, couldBeValidNumber } from '../../util';
 
 export interface IPieOpt {
   angleField: string;
@@ -15,6 +15,13 @@ export interface IPieOpt {
   asRatio: string;
   asQuadrant: string;
   asK: string;
+}
+
+function transformInvalidValue(value: any) {
+  if (!couldBeValidNumber(value)) {
+    return 0;
+  }
+  return Number.parseFloat(value);
 }
 
 export const pie = (originData: Array<DataView>, op: IPieOpt) => {
@@ -35,13 +42,13 @@ export const pie = (originData: Array<DataView>, op: IPieOpt) => {
     asK
   } = op;
 
-  const total = data.reduce((sum, d) => Number.parseFloat(d[angleField]) + sum, 0);
-  const max = data.reduce((m, d) => Math.max(m, Number.parseFloat(d[angleField])), -Infinity);
+  const total = data.reduce((sum, d) => transformInvalidValue(d[angleField]) + sum, 0);
+  const max = data.reduce((m, d) => Math.max(m, transformInvalidValue(d[angleField])), -Infinity);
 
   const intervalAngle = endAngle - startAngle;
   let lastAngle = startAngle;
   data.forEach(d => {
-    const ratio = Number.parseFloat(d[angleField]) / total;
+    const ratio = total ? transformInvalidValue(d[angleField]) / total : 0;
     const radian = ratio * intervalAngle;
 
     asRatio && (d[asRatio] = ratio);
@@ -50,7 +57,7 @@ export const pie = (originData: Array<DataView>, op: IPieOpt) => {
     asMiddleAngle && (d[asMiddleAngle] = lastAngle + radian / 2);
     asRadian && (d[asRadian] = radian);
     asQuadrant && (d[asQuadrant] = computeQuadrant(lastAngle + radian / 2));
-    asK && (d[asK] = Number.parseFloat(d[angleField]) / max);
+    asK && (d[asK] = max ? transformInvalidValue(d[angleField]) / max : 0);
 
     lastAngle = d[asEndAngle];
   });
