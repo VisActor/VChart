46a116fc53ec52ded4c25b6fa5db0fc2126379aa
Howard Zhang <zamhown@live.com>
2023-09-04 15:37:28 +0800

    fix: type problem

--- packages/vchart/src/util/text.ts
+++ packages/vchart/src/util/text.ts
@@ -2,15 +2,16 @@ import type { ITextMeasureOption } from '@visactor/vutils';
 // eslint-disable-next-line no-duplicate-imports
 import { TextMeasure } from '@visactor/vutils';
 import { getTextBounds } from '@visactor/vrender';
-import type { ITextMarkSpec } from '../typings';
+import type { AdaptiveSpec, ITextMarkSpec } from '../typings';
 import { THEME_CONSTANTS } from '../theme/builtin/common/constants';
 
 export const initTextMeasure = (
   textSpec?: Partial<ITextMarkSpec>,
   option?: Partial<ITextMeasureOption>,
   useNaiveCanvas?: boolean
-): TextMeasure<ITextMarkSpec> => {
-  return new TextMeasure<ITextMarkSpec>(
+): TextMeasure<AdaptiveSpec<ITextMarkSpec, 'lineHeight'>> => {
+  // FIXME: vrender 支持行高字符串后删除 AdaptiveSpec 范型
+  return new TextMeasure<AdaptiveSpec<ITextMarkSpec, 'lineHeight'>>(
     {
       defaultFontParams: {
         fontFamily: THEME_CONSTANTS.defaultFontFamily,
