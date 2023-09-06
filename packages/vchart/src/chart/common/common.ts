b25ff6afdc2bfbcb1e7cfab7d310dc580d785f72
xiaoluoHe <sevnape@outlook.com>
2023-09-06 11:30:37 +0800

    fix: animation config not work in common chart, close #814

--- packages/vchart/src/chart/common/common.ts
+++ packages/vchart/src/chart/common/common.ts
@@ -1,8 +1,27 @@
+import { isArray } from '@visactor/vutils';
 import { BaseChart } from '../base-chart';
 import { ChartTypeEnum } from '../interface';
+import type { ISeries } from '../../series';
 
 export class CommonChart extends BaseChart {
   static readonly type: string = ChartTypeEnum.common;
   static readonly view: string = 'singleDefault';
   readonly type: string = ChartTypeEnum.common;
+
+  transformSpec(spec: any): void {
+    super.transformSpec(spec);
+    if (isArray(spec.series)) {
+      const defaultSeriesSpec = this._getDefaultSeriesSpec(spec);
+      spec.series.forEach((s: ISeries) => {
+        if (!this.isValidSeries(s.type)) {
+          return;
+        }
+        Object.keys(defaultSeriesSpec).forEach(k => {
+          if (!(k in s)) {
+            s[k] = defaultSeriesSpec[k];
+          }
+        });
+      });
+    }
+  }
 }
