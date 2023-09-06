8f4bf3f65a493211a7e84ed3f463b27f759590db
skie1997 <skie1997@outlook.com>
2023-09-06 22:14:18 +0800

    fix(sequence): add scrollbar layout spec to sequence. fix #792

---
+++ packages/vchart/__tests__/data/data-sequence-event-link.ts
@@ -0,0 +1,637 @@
+export const dataEventLink = [
+  {
+    from: '683827468797481xxxx_1662346668_im',
+    to: '683827420033672xxxx_1662346668_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662480000_combine',
+    to: '683827420033672xxxx_1662480000_combine',
+    action_type: 'combine',
+    children: [
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'switch_accoount'
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'invite',
+        placeholder: 'placeholder'
+      },
+      {
+        action_type: 'follow',
+        is_unfollow: false,
+        source: 1
+      },
+      {
+        action_type: 'like',
+        video_id: 1234567890,
+        is_cancel: true
+      },
+      {
+        action_type: 'report',
+        content: 'report context in string',
+        report_type: 'user',
+        target: 'user_id'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'enter_personal_detail'
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      },
+      {
+        action_type: 'post_video',
+        video_id: 987654,
+        is_delete: true
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662380000_combine',
+    to: '683827420033672xxxx_1662380000_comment',
+    action_type: 'combine',
+    children: [
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      },
+      {
+        action_type: 'change_profile',
+        old_signature: '1',
+        new_signature: '2',
+        old_nickname: '3',
+        new_nickname: '4',
+        old_avatar_uri: '5',
+        new_avatar_uri: '6'
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662280000_combine',
+    to: '683827420033672xxxx_1662280000_comment',
+    action_type: 'combine',
+    children: [
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      },
+      {
+        action_type: 'change_profile',
+        old_signature: '1',
+        new_signature: '2',
+        old_nickname: '3',
+        new_nickname: '4',
+        old_avatar_uri: '5',
+        new_avatar_uri: '6'
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662246669_im',
+    to: '683827420033672xxxx_1662246669_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662380001_im',
+    to: '683827420033672xxxx_1662380001_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662130000_comment',
+    to: '683827420033672xxxx_1662130000_comment',
+    action_type: 'comment',
+    children: [
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662080000_combine',
+    to: '683827420033672xxxx_1662080000_comment',
+    action_type: 'combine',
+    children: [
+      {
+        action_type: 'change_profile',
+        old_signature: '1',
+        new_signature: '2',
+        old_nickname: '3',
+        new_nickname: '4',
+        old_avatar_uri: '5',
+        new_avatar_uri: '6'
+      },
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662413334_im',
+    to: '683827420033672xxxx_1662413334_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662313335_im',
+    to: '683827420033672xxxx_1662313335_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '25328254226993xxxx_1662321122_login_device',
+    to: '683827422612367xxxx_1662321122_login_device',
+    action_type: 'login_device',
+    children: [
+      {
+        action_type: 'login_device'
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662301120_registry_device',
+    to: '683827422612367xxxx_1662301120_registry_device',
+    action_type: 'registry_device',
+    children: [
+      {
+        action_type: 'registry_device'
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662361127_registry_ip',
+    to: '93.23.xxx.xx_20200615_1662361127_registry_ip',
+    action_type: 'registry_ip',
+    children: [
+      {
+        action_type: 'registry_ip'
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662230000_comment',
+    to: '683827420033672xxxx_1662230000_comment',
+    action_type: 'comment',
+    children: [
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      }
+    ]
+  },
+  {
+    from: '713019589280868xxxx_1662061124_login_device',
+    to: '713019502467512xxxx_1662061124_login_device',
+    action_type: 'login_device',
+    children: [
+      {
+        action_type: 'login_device'
+      }
+    ]
+  },
+  {
+    from: '683827420033672xxxx_1662503541_registry_device',
+    to: '683827422612367xxxx_1662503541_registry_device',
+    action_type: 'registry_device',
+    children: [
+      {
+        action_type: 'registry_device'
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662180000_combine',
+    to: '683827420033672xxxx_1662180000_comment',
+    action_type: 'combine',
+    children: [
+      {
+        action_type: 'change_profile',
+        old_signature: '1',
+        new_signature: '2',
+        old_nickname: '3',
+        new_nickname: '4',
+        old_avatar_uri: '5',
+        new_avatar_uri: '6'
+      },
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662430000_comment',
+    to: '683827420033672xxxx_1662430000_comment',
+    action_type: 'comment',
+    children: [
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662446667_im',
+    to: '683827420033672xxxx_1662446667_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662331123_binding_email',
+    to: 'bcb395f84ae8882de39d2a8e68d459c177a7b5a0b6884047b0fce1f28d21xxxx_1662331123_binding_email',
+    action_type: 'binding_email',
+    children: [
+      {
+        action_type: 'binding_email'
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662280002_im',
+    to: '683827420033672xxxx_1662280002_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662180003_im',
+    to: '683827420033672xxxx_1662180003_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827957407865xxxx_1662311121_login_device',
+    to: '683827422612367xxxx_1662311121_login_device',
+    action_type: 'login_device',
+    children: [
+      {
+        action_type: 'login_device'
+      }
+    ]
+  },
+  {
+    from: null,
+    to: null
+  },
+  {
+    from: '683827468797481xxxx_1662213336_im',
+    to: '683827420033672xxxx_1662213336_im',
+    action_type: 'im',
+    children: [
+      {
+        action_type: 'im',
+        msg_id: 800000
+      }
+    ]
+  },
+  {
+    from: '683827468797481xxxx_1662330000_comment',
+    to: '683827420033672xxxx_1662330000_comment',
+    action_type: 'comment',
+    children: [
+      {
+        action_type: 'comment',
+        comment_id: 3000,
+        content: 'some comment from tiktok user',
+        video_id: 90000001,
+        is_delete: false
+      }
+    ]
+  },
+  {
+    from: '683827420033672xxxx_1662161125_login_device',
+    to: '683827422612367xxxx_1662161125_login_device',
+    action_type: 'login_device',
+    children: [
+      {
+        action_type: 'login_device'
+      }
+    ]
+  }
+];
