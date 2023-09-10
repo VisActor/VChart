848c6799e1d2f696f03ff1f675e4ecc7b588b5aa
Howard Zhang <zamhown@live.com>
2023-09-11 07:36:12 +0800

    fix: unit test type problem

--- packages/vchart/src/component/axis/mixin/band-axis-mixin.ts
+++ packages/vchart/src/component/axis/mixin/band-axis-mixin.ts
@@ -26,7 +26,7 @@ export class BandAxisMixin {
     let parentScale = this._scale;
     this._scales.forEach((scale, i) => {
       if (i > 0) {
-        scale.range([0, parentScale.bandwidth()], false, true);
+        scale.range([0, parentScale.bandwidth()], false);
         parentScale = scale;
       }
     });
