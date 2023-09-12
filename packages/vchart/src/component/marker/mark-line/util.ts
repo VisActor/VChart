31105fd477a24690e860de52fff5dcd871ef7b9b
kkxxkk2019 <just_star@qq.com>
2023-09-12 22:30:36 +0800

    feat: support a new type of markLine, which defined by `type: 'type-step'`

---
+++ packages/vchart/src/component/marker/mark-line/util.ts
@@ -0,0 +1,98 @@
+import type { IPointLike } from '@visactor/vutils';
+
+export function getInsertPoints(
+  start: IPointLike,
+  end: IPointLike,
+  direction: 'top' | 'bottom' | 'left' | 'right',
+  offset: number = 0
+) {
+  const points: IPointLike[] = [];
+  const dy = start.y - end.y;
+  const dx = start.x - end.x;
+
+  switch (direction) {
+    case 'top':
+      points.push(start);
+      points.push({
+        x: start.x,
+        y: dy > 0 ? start.y - offset - Math.abs(dy) : start.y - offset
+      });
+      points.push({
+        x: end.x,
+        y: dy > 0 ? end.y - offset : end.y - offset - Math.abs(dy)
+      });
+      points.push(end);
+      break;
+    case 'bottom':
+      points.push(start);
+      points.push({ x: start.x, y: dy < 0 ? start.y + offset + Math.abs(dy) : start.y + offset });
+      points.push({ x: end.x, y: dy < 0 ? end.y + offset : end.y + offset + Math.abs(dy) });
+      points.push(end);
+      break;
+    case 'left':
+      points.push(start);
+      points.push({
+        x: dx > 0 ? start.x - offset - Math.abs(dx) : start.x - offset,
+        y: start.y
+      });
+      points.push({
+        x: dx > 0 ? end.x - offset : end.x - offset - Math.abs(dx),
+        y: end.y
+      });
+      points.push(end);
+      break;
+    case 'right':
+      points.push(start);
+      points.push({
+        x: dx > 0 ? start.x + offset : start.x + offset + Math.abs(dx),
+        y: start.y
+      });
+      points.push({
+        x: dx > 0 ? end.x + offset + Math.abs(dx) : end.x + offset,
+        y: end.y
+      });
+      points.push(end);
+      break;
+    default:
+      break;
+  }
+  return points;
+}
+
+export function getTextOffset(
+  start: IPointLike,
+  end: IPointLike,
+  direction: 'top' | 'bottom' | 'left' | 'right',
+  offset: number = 0
+) {
+  const dy = start.y - end.y;
+  const dx = start.x - end.x;
+
+  if (direction === 'bottom') {
+    return {
+      dx: dx > 0 ? -dx / 2 : Math.abs(dx / 2),
+      dy: dy > 0 ? offset : Math.abs(dy) + offset
+    };
+  }
+
+  if (direction === 'top') {
+    return {
+      dx: dx > 0 ? -Math.abs(dx / 2) : +Math.abs(dx / 2),
+      dy: dy > 0 ? -(Math.abs(dy) + offset) : -offset
+    };
+  }
+
+  if (direction === 'left') {
+    return {
+      dx: dx > 0 ? -dx - offset : -offset,
+      dy: dy > 0 ? -(dy / 2) : Math.abs(dy / 2)
+    };
+  }
+
+  if (direction === 'right') {
+    return {
+      dx: dx > 0 ? offset : Math.abs(dx) + offset,
+      dy: dy > 0 ? -(dy / 2) : Math.abs(dy / 2)
+    };
+  }
+}
