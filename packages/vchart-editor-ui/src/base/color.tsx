c039324adaf911f0505f0e0e8b4555f72468f0c5
purpose233 <zhangsu.melon@gmail.com>
2023-09-14 21:21:06 +0800

    feat: add entries in title editor

---
+++ packages/vchart-editor-ui/src/base/color.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { SketchPicker } from 'react-color';
+import type { IBaseColorComponentProps } from '../typings/base';
+import { defaultBaseComponentConfig } from '../config/base';
+import { Button, Popover } from '@douyinfe/semi-ui';
+
+export function Color(props: IBaseColorComponentProps) {
+  const label = props.label ?? defaultBaseComponentConfig.color.label;
+  const defaultColor = props.color ?? defaultBaseComponentConfig.color.default;
+  const [color, setColor] = useState<string>(defaultColor);
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <p>{label}</p>
+      <Popover
+        content={
+          <SketchPicker
+            color={color}
+            onChange={color => {
+              setColor(color.hex);
+              props.onChange?.(color.hex);
+            }}
+          />
+        }
+      >
+        <Button>
+          <div style={{ width: 40, height: 20, backgroundColor: color }}></div>
+        </Button>
+      </Popover>
+    </div>
+  );
+}
