22a382f94bd3eff66da80238ed2877f06da29e40
purpose233 <zhangsu.melon@gmail.com>
2023-09-14 19:25:12 +0800

    feat: support config in editor components

--- packages/vchart-editor-ui/src/components/title.tsx
+++ packages/vchart-editor-ui/src/components/title.tsx
@@ -1,27 +1,60 @@
 import { FontFamily } from '../base/font-family';
-import { FontSize } from '../base/font-size';
 import { FontStyle } from '../base/font-style';
+import { SliderNumber } from '../base/slider-number';
 import type { ITitleComponentProps } from '../typings/components';
+import type { ITitleEditorComponentConfig } from '../typings/config';
+
+const titleDefaultEntries: ITitleEditorComponentConfig['entries'] = [
+  { key: 'fontSize', label: '字号' },
+  { key: 'fontFamily', label: '字体' },
+  { key: 'fontStyle', label: '样式' }
+];
 
 export function Title(props: ITitleComponentProps) {
   const onChange = (key: string, value: any) => {
     // console.log(key, value);
     props.onChange?.();
   };
+
+  const entries = props.config?.entries ?? titleDefaultEntries;
+
   return (
     <div>
-      <p>Title</p>
-      <FontFamily
-        fontFamily="PingFangSC-Regular"
-        onChange={(fontFamily: number) => onChange('fontFamily', fontFamily)}
-      />
-      <FontSize fontSize={10} onChange={(fontSize: number) => onChange('fontSize', fontSize)} />
-      <FontStyle
-        bolder={false}
-        underline={false}
-        italic={false}
-        onChange={(fontStyle: any) => onChange('fontStyle', fontStyle)}
-      />
+      <p>图表标题</p>
+      {entries.map((entry, index) => {
+        switch (entry.key) {
+          case 'fontSize':
+            return (
+              <SliderNumber
+                key={`${entry.key}-${index}`}
+                label={entry.label}
+                value={10}
+                onChange={(fontSize: number) => onChange('fontSize', fontSize)}
+              />
+            );
+          case 'fontFamily':
+            return (
+              <FontFamily
+                key={`${entry.key}-${index}`}
+                label={entry.label}
+                fontFamily="PingFangSC-Regular"
+                onChange={(fontFamily: string) => onChange('fontFamily', fontFamily)}
+              />
+            );
+          case 'fontStyle':
+            return (
+              <FontStyle
+                key={`${entry.key}-${index}`}
+                label={entry.label}
+                bolder={false}
+                underline={false}
+                italic={false}
+                onChange={(fontStyle: any) => onChange('fontStyle', fontStyle)}
+              />
+            );
+        }
+        return null;
+      })}
     </div>
   );
 }
---
+++ packages/vchart-editor-ui/src/config/editor.ts
@@ -0,0 +1,10 @@
+import type { ITitleEditorComponentConfig } from '../typings/config';
+
+const titleDefaultConfig: ITitleEditorComponentConfig = {
+  label: '图标标题',
+  entries: [
+    { key: 'fontSize', label: '字号' },
+    { key: 'fontFamily', label: '字体' },
+    { key: 'fontStyle', label: '样式' }
+  ]
+};
