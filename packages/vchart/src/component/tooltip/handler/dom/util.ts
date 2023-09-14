8b022d077dccddee840acb89e5cfd70884859b1a
Howard Zhang <zamhown@live.com>
2023-09-14 20:13:40 +0800

    fix: shape bounds

--- packages/vchart/src/component/tooltip/handler/dom/util.ts
+++ packages/vchart/src/component/tooltip/handler/dom/util.ts
@@ -20,6 +20,20 @@ export const getPixelPropertyStr = (num?: number | number[], defaultStr?: string
   return defaultStr ?? 'initial';
 };
 
+export const pixelPropertyStrToNumber = (str: string): number | number[] => {
+  const strArr = str.split(' ');
+  const numArr = strArr.map(n => {
+    if (!Number.isNaN(n)) {
+      return Number.parseFloat(n);
+    }
+    return Number.parseFloat(n.substring(0, n.length - 2));
+  });
+  if (numArr.length === 1) {
+    return numArr[0];
+  }
+  return numArr;
+};
+
 export function getDomStyles(
   style: Partial<ITooltipStyle>,
   attributeCache?: Maybe<TooltipAttributes>
