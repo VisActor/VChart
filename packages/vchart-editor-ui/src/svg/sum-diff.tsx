bac71f13262d75006e2b76f3a2475e2cf4de8c6d
purpose233 <zhangsu.melon@gmail.com>
2023-09-19 00:33:31 +0800

    feat: add svg assets

---
+++ packages/vchart-editor-ui/src/svg/sum-diff.tsx
@@ -0,0 +1,36 @@
+export function IconSumDiff() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <rect
+        x="2.1676"
+        y="7.26367"
+        width="4.6145"
+        height="10.019"
+        rx="0.555556"
+        stroke="#2B2F36"
+        strokeWidth="1.66667"
+      />
+      <rect
+        x="12.6196"
+        y="11.5996"
+        width="4.6145"
+        height="5.68311"
+        rx="0.555556"
+        stroke="#2B2F36"
+        strokeWidth="1.66667"
+      />
+      <path
+        // eslint-disable-next-line
+        d="M14.9269 9.16278V2.83095C14.9269 2.52412 14.6781 2.27539 14.3713 2.27539H5.03688C4.73005 2.27539 4.48132 2.52412 4.48132 2.83095V6.1756"
+        stroke="#2B2F36"
+        strokeWidth="0.8"
+        strokeDasharray="1.5 1.5"
+      />
+      <path
+        // eslint-disable-next-line
+        d="M14.777 10.5495C14.8573 10.6485 15.0581 10.6485 15.1384 10.5495L17.2675 7.92398C17.3478 7.82495 17.2474 7.70117 17.0868 7.70117L12.8286 7.70117C12.668 7.70117 12.5676 7.82495 12.6479 7.92398L14.777 10.5495Z"
+        fill="#2B2F36"
+      />
+    </svg>
+  );
+}
