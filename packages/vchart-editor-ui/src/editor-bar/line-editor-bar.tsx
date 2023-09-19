d4d7f979041cf20f661eef8e5391842acebb2861
purpose233 <zhangsu.melon@gmail.com>
2023-09-19 20:15:53 +0800

    feat: align the style of editor bar

--- packages/vchart-editor-ui/src/editor-bar/line-editor-bar.tsx
+++ packages/vchart-editor-ui/src/editor-bar/line-editor-bar.tsx
@@ -1,17 +1,15 @@
 import { Divider } from '@douyinfe/semi-ui';
-import { IconBold } from '@douyinfe/semi-icons';
 import type { IEditorComponentProps } from '../typings/editor-bar';
 import { IconComment } from '../svg/comment';
 import { EditorBarStrokeLine } from './stroke';
+import { EditorBarTool } from './util';
 
 export function LineEditorBar(props: IEditorComponentProps) {
   return (
     <div className="vchart-editor-ui-editor-bar-container" style={{ ...(props.style ?? {}) }}>
-      <IconBold />
-      <Divider layout="vertical" margin="12px" />
       <EditorBarStrokeLine strokeStyle="disable" strokeColor="#000000" strokeWidth={1} strokeOpacity={1} />
-      <Divider layout="vertical" margin="12px" />
-      <IconComment />
+      <Divider layout="vertical" margin="8px" style={{ height: 10 }} />
+      <EditorBarTool icon={<IconComment />} selected={false} />
     </div>
   );
 }
