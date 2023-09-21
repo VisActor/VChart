fde87594593bd22257e51ef7bb71101ccbe5c7b7
skie1997 <skie1997@outlook.com>
2023-09-22 02:17:03 +0800

    fix(wordcloud): fontsize renge not effect with no value field. fix #522

--- packages/vchart/src/series/word-cloud/base.ts
+++ packages/vchart/src/series/word-cloud/base.ts
@@ -62,7 +62,6 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
   setValueField(field: string) {
     if (isValid(field)) {
       this._valueField = field;
-      this.setFontSizeRange(DEFAULT_FONTSIZE_RANGE);
     }
   }
 
@@ -77,8 +76,10 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
 
   protected _fontSizeRange?: [number, number] = [DEFAULT_MIN_FONT_SIZE, DEFAULT_MIN_FONT_SIZE];
   setFontSizeRange(fontSizeRange: [number, number]) {
-    if (isValid(fontSizeRange) && isValid(this._spec.valueField)) {
+    if (isValid(fontSizeRange)) {
       this._fontSizeRange = fontSizeRange;
+    } else {
+      this._fontSizeRange = DEFAULT_FONTSIZE_RANGE;
     }
   }
 
