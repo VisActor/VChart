fa19ceb108e56b68e4d3a45e9b811753fde4c11d
lixuefei.1313 <lixuef1313@163.com>
2023-09-22 15:29:43 +0800

    feat: increase chart stacking capabilities and provide stackValue to support independent stacking of multiple series

--- packages/vchart/src/typings/spec/common.ts
+++ packages/vchart/src/typings/spec/common.ts
@@ -205,6 +205,13 @@ export interface IChartSpec {
   // TODO：后续开放，现在仍有问题
   // poptip?: PopTipAttributes;
   // TODO: 补充动画配置
+
+  /**
+   * 堆积时是否逆序
+   * @default false
+   * @since 1.4.1
+   */
+  stackInverse?: boolean;
 }
 
 export type IBackgroundStyleSpec = ConvertToMarkStyleSpec<Omit<IFillMarkSpec, 'width' | 'height' | 'background'>> & {
@@ -364,6 +371,13 @@ export interface ISeriesSpec extends ITriggerSpec {
 
   /** 是否对数据进行堆叠处理 */
   stack?: boolean;
+
+  /**
+   * 堆叠时的分组值
+   * stackValue 相等的系列将在一起堆积。没有配置的系列将在一组
+   * @since 1.4.1
+   */
+  stackValue?: StringOrNumber;
 
   /** 堆叠汇总标签
    * @since 1.3.0
