f03b64f6cd6089d73425380b1723c4aaab2cb808
lixuefei.1313 <lixuef1313@163.com>
2023-09-27 10:54:51 +0800

    feat: change editor logic with editor-element

---
+++ packages/vchart-editor/src/elements/chart/editor-elements/base-editor-element.ts
@@ -0,0 +1,23 @@
+import type { IEditorController, IEditorElement, IEditorLayer } from './../../../core/interface';
+import type { EditorChart } from '../chart';
+import type { IGraphic } from '@visactor/vrender';
+
+export abstract class BaseEditorElement {
+  protected _chart: EditorChart;
+  protected _controller: IEditorController;
+  protected _layer: IEditorLayer;
+
+  protected _overGraphic: IGraphic;
+  protected _currentEl: IEditorElement;
+
+  constructor(controller: IEditorController, chart: EditorChart, layer: IEditorLayer) {
+    this._controller = controller;
+    this._chart = chart;
+    this._layer = layer;
+  }
+
+  abstract initWithVChart(): void;
+  protected abstract _createEditorGraphic(el: IEditorElement, e?: PointerEvent): IGraphic;
+  protected abstract _getOverGraphic(el: IEditorElement, e?: PointerEvent): IGraphic;
+  protected abstract _getEditorElement(e?: PointerEvent): IEditorElement;
+}
