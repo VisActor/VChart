52f871355861ad6727fe7019ffa03a3c48046b63
kkxxkk2019 <just_star@qq.com>
2023-10-16 09:55:30 +0800

    feat(vchart-editor): add the interaction of total diff marker line

--- packages/vchart-editor/src/elements/chart/editor-elements/growth-mark-line.ts
+++ packages/vchart-editor/src/elements/chart/editor-elements/growth-mark-line.ts
@@ -1,24 +1,30 @@
 /**
- * @description 复合增长标注标记交互
+ * @description 复合增长 & 总计差异标注标记交互
  * 1. 保存位置 & 更新 spec，直接更新 spec
  * 2. 双击出现编辑框
  */
-import type { IGroup, ILine } from '@visactor/vrender-core';
+import type { IGroup, ILine, ISymbol } from '@visactor/vrender-core';
 import { type IGraphic, createGroup, vglobal, createLine, createSymbol } from '@visactor/vrender-core';
 import type { IEditorElement } from '../../../core/interface';
 import { BaseEditorElement, CommonChartEditorElement } from './base-editor-element';
-import { IPointLike, array, has, merge } from '@visactor/vutils';
+import { IPointLike, array, has, last, merge } from '@visactor/vutils';
 import type { MarkLine as MarkLineComponent } from '@visactor/vrender-components';
 import { Segment } from '@visactor/vrender-components';
-import type { EventParams, MarkLine, ISeries, ICartesianSeries, IComponent } from '@visactor/vchart';
-import { STACK_FIELD_TOTAL_TOP } from '@visactor/vchart';
+import type { EventParams, MarkLine } from '@visactor/vchart';
+import { DEFAULT_DATA_KEY, STACK_FIELD_TOTAL_TOP } from '@visactor/vchart';
 import type { Point } from '@visactor/vrender-components/es/core/type';
 import { findClosestPoint } from '../utils/math';
+import { getInsertPoints, getTextOffset } from '../utils/marker';
+// TODO： 类型导出
+import { IStepMarkLineSpec } from '@visactor/vchart/src/component/marker/mark-line';
+import { ICartesianSeries } from '@visactor/vchart/src/series/interface';
+import { IComponent } from '@visactor/vchart/src/component/interface';
 
 type DataPoint = {
   x: number;
   y: number;
-  data: any[];
+  data: any;
+  length: number;
 };
 
 const START_LINK_HANDLER = 'overlay-growth-mark-line-start-handler';
@@ -30,10 +36,10 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
 
   private _editComponent: IGroup;
   private _overlayLine: ILine;
-  private _overlayStartHandler: Segment;
-  private _overlayEndHandler: Segment;
-  private _currentHandler: Segment;
-  private _fixedHandler: Segment;
+  private _overlayStartHandler: IGraphic;
+  private _overlayEndHandler: IGraphic;
+  private _currentHandler: IGraphic;
+  private _fixedHandler: IGraphic;
   private _dataAnchors: IGroup;
 
   private _selected: boolean = false;
@@ -48,7 +54,7 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
 
   private _checkEventEnable(e) {
     const markerComponent = e.model.getVRenderComponents()[0];
-    return markerComponent?.name === 'growthMarkLine';
+    return markerComponent?.name === 'growthMarkLine' || markerComponent?.name === 'totalDiffMarkLine';
   }
 
   private _onHover = (e: any) => {
@@ -94,8 +100,8 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
         },
         pickable: false,
         childrenPickable: false,
-        dx: model.getSpec().offsetX ?? 0,
-        dy: model.getSpec().offsetY ?? 0
+        dx: markLine.attribute.dx ?? 0,
+        dy: markLine.attribute.dy ?? 0
       })
     );
     overlayLine.name = 'overlay-growth-mark-line-line';
@@ -137,82 +143,123 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
       lineDash: [0],
       lineWidth: 3,
       stroke: '#3073F2',
-      dx: model.getSpec().offsetX ?? 0,
-      dy: model.getSpec().offsetY ?? 0,
+      dx: this._element.attribute.dx ?? 0,
+      dy: this._element.attribute.dy ?? 0,
       pickable: false
     });
     overlayLine.name = 'overlay-growth-mark-line-line';
     this._overlayLine = overlayLine;
     editComponent.add(overlayLine);
 
-    const relativeDataPoints = (lineShape.attribute.points as Point[]).map(point => {
+    const relativeDataPoints = [
+      (lineShape.attribute.points as Point[])[0],
+      last(lineShape.attribute.points as Point[])
+    ].map(point => {
       return dataPoints.find((dataPoint: DataPoint) => dataPoint.x === point.x && dataPoint.y === point.y);
     });
 
-    const startLinkLine = new Segment({
-      points: [
-        relativeDataPoints[0],
-        {
-          x: relativeDataPoints[0].x + (model.getSpec().offsetX ?? 0),
-          y: relativeDataPoints[0].y + (model.getSpec().offsetY ?? 0)
-        }
-      ],
-      startSymbol: {
-        visible: true,
+    let startLinkLine;
+    let endLinkLine;
+    if (this._element.name === 'growthMarkLine') {
+      startLinkLine = new Segment({
+        zIndex: 1,
+        points: [
+          relativeDataPoints[0],
+          {
+            x: relativeDataPoints[0].x + (this._element.attribute.dx ?? 0),
+            y: relativeDataPoints[0].y + (this._element.attribute.dy ?? 0)
+          }
+        ],
+        startSymbol: {
+          visible: true,
+          symbolType: 'circle',
+          size: 10,
+          style: {
+            fill: '#fff',
+            stroke: '#3073F2',
+            lineWidth: 1,
+            shadowBlur: 4,
+            shadowOffsetX: 0,
+            shadowOffsetY: 4,
+            shadowColor: 'rgba(0, 0, 0, 0.25)'
+          }
+        },
+        endSymbol: { visible: false },
+        lineStyle: {
+          stroke: '#89909D',
+          lineWidth: 1
+        },
+        childrenPickable: false
+      });
+
+      endLinkLine = new Segment({
+        zIndex: 1,
+        points: [
+          relativeDataPoints[1],
+          {
+            x: relativeDataPoints[1].x + (this._element.attribute.dx ?? 0),
+            y: relativeDataPoints[1].y + (this._element.attribute.dy ?? 0)
+          }
+        ],
+        startSymbol: {
+          visible: true,
+          symbolType: 'circle',
+          size: 10,
+          style: {
+            fill: '#fff',
+            stroke: '#3073F2',
+            lineWidth: 1,
+            shadowBlur: 4,
+            shadowOffsetX: 0,
+            shadowOffsetY: 4,
+            shadowColor: 'rgba(0, 0, 0, 0.25)'
+          }
+        },
+        endSymbol: { visible: false },
+        lineStyle: {
+          stroke: '#89909D',
+          lineWidth: 1
+        },
+        childrenPickable: false
+      });
+    } else {
+      startLinkLine = createSymbol({
+        zIndex: 1,
+        x: relativeDataPoints[0].x,
+        y: relativeDataPoints[0].y,
         symbolType: 'circle',
         size: 10,
-        style: {
-          fill: '#fff',
-          stroke: '#3073F2',
-          lineWidth: 1,
-          shadowBlur: 4,
-          shadowOffsetX: 0,
-          shadowOffsetY: 4,
-          shadowColor: 'rgba(0, 0, 0, 0.25)'
-        }
-      },
-      endSymbol: { visible: false },
-      lineStyle: {
-        stroke: '#89909D',
-        lineWidth: 1
-      },
-      childrenPickable: false
-    });
+        fill: '#fff',
+        stroke: '#3073F2',
+        lineWidth: 1,
+        shadowBlur: 4,
+        shadowOffsetX: 0,
+        shadowOffsetY: 4,
+        shadowColor: 'rgba(0, 0, 0, 0.25)'
+      });
+
+      endLinkLine = createSymbol({
+        zIndex: 1,
+        x: relativeDataPoints[1].x,
+        y: relativeDataPoints[1].y,
+        symbolType: 'circle',
+        size: 10,
+        fill: '#fff',
+        stroke: '#3073F2',
+        lineWidth: 1,
+        shadowBlur: 4,
+        shadowOffsetX: 0,
+        shadowOffsetY: 4,
+        shadowColor: 'rgba(0, 0, 0, 0.25)'
+      });
+    }
+
     startLinkLine.name = START_LINK_HANDLER;
-    this._overlayStartHandler = startLinkLine;
+    this._overlayStartHandler = startLinkLine as unknown as IGraphic;
     editComponent.add(startLinkLine as unknown as IGraphic);
 
-    const endLinkLine = new Segment({
-      points: [
-        relativeDataPoints[1],
-        {
-          x: relativeDataPoints[1].x + (model.getSpec().offsetX ?? 0),
-          y: relativeDataPoints[1].y + (model.getSpec().offsetY ?? 0)
-        }
-      ],
-      startSymbol: {
-        visible: true,
-        symbolType: 'circle',
-        size: 10,
-        style: {
-          fill: '#fff',
-          stroke: '#3073F2',
-          lineWidth: 1,
-          shadowBlur: 4,
-          shadowOffsetX: 0,
-          shadowOffsetY: 4,
-          shadowColor: 'rgba(0, 0, 0, 0.25)'
-        }
-      },
-      endSymbol: { visible: false },
-      lineStyle: {
-        stroke: '#89909D',
-        lineWidth: 1
-      },
-      childrenPickable: false
-    });
     endLinkLine.name = END_LINK_HANDLER;
-    this._overlayEndHandler = endLinkLine;
+    this._overlayEndHandler = endLinkLine as unknown as IGraphic;
     editComponent.add(endLinkLine as unknown as IGraphic);
 
     this._layer.editorGroup.add(editComponent as unknown as IGraphic);
@@ -252,7 +299,7 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
   private _onHandlerDragStart = (e: any) => {
     e.stopPropagation();
     const handler = e.target;
-    this._currentHandler = handler as unknown as Segment;
+    this._currentHandler = handler as unknown as IGraphic;
     this._fixedHandler = handler.name === START_LINK_HANDLER ? this._overlayEndHandler : this._overlayStartHandler;
 
     vglobal.addEventListener('pointermove', this._onHandlerDrag);
@@ -271,7 +318,13 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
     // 展示可吸附的数据锚点
     // 对于数据锚点，不允许拖拽至另一个固定的锚点
     const enableDataPoints: any[] = [];
-    const unenableDataPoint = this._fixedHandler.attribute.points[0] as Point;
+    const unenableDataPoint =
+      this._element.name === 'growthMarkLine'
+        ? ((this._fixedHandler as unknown as Segment).attribute.points[0] as Point)
+        : {
+            x: (this._fixedHandler as unknown as ISymbol).attribute.x,
+            y: (this._fixedHandler as unknown as ISymbol).attribute.y
+          };
     this._dataAnchors.getChildren().forEach((child: any) => {
       if (child.attribute.x === unenableDataPoint.x && child.attribute.y === unenableDataPoint.y) {
         child.setAttribute('visible', false);
@@ -290,30 +343,77 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
     // 转换为画布坐标
     const stage = this._chart.vchart.getStage();
 
+    // TODO: 修改为公共属性
     const currentPoint = stage.global.mapToCanvasPoint(e);
-    const closestPoint = findClosestPoint(currentPoint, enableDataPoints);
-
-    this._currentHandler.setAttributes({
-      points: [
-        closestPoint,
-        {
-          x: closestPoint.x + (this._model.getSpec().offsetX ?? 0),
-          y: closestPoint.y + (this._model.getSpec().offsetY ?? 0)
-        }
-      ]
-    });
+    const closestPoint = findClosestPoint(currentPoint, enableDataPoints) as Point;
     // @ts-ignore
     this._currentHandler.data = closestPoint.data;
+    if (this._element.name === 'growthMarkLine') {
+      const offset = this._getMaxLengthBetweenHandlers();
+      // TODO: 30 变常量
+      (this._currentHandler as unknown as Segment).setAttributes({
+        points: [
+          closestPoint,
+          {
+            x: closestPoint.x + (this._model.getRelativeSeries().direction === 'horizontal' ? offset + 30 : 0),
+            y: closestPoint.y + (this._model.getRelativeSeries().direction === 'horizontal' ? 0 : -offset - 30)
+          }
+        ]
+      });
+      (this._fixedHandler as unknown as Segment).setAttributes({
+        points: [
+          (this._fixedHandler as unknown as Segment).attribute.points[0] as Point,
+          {
+            x:
+              ((this._fixedHandler as unknown as Segment).attribute.points[0] as Point).x +
+              (this._model.getRelativeSeries().direction === 'horizontal' ? offset + 30 : 0),
+            y:
+              ((this._fixedHandler as unknown as Segment).attribute.points[0] as Point).y +
+              (this._model.getRelativeSeries().direction === 'horizontal' ? 0 : -offset - 30)
+          }
+        ]
+      });
+      let attrKey;
+      let attrValue;
+      if (this._model.getRelativeSeries().direction === 'horizontal') {
+        attrKey = 'dx';
+        attrValue = offset + 30;
+      } else {
+        attrKey = 'dy';
+        attrValue = -offset - 30;
+      }
+      this._overlayLine.setAttributes({
+        points: [
+          closestPoint,
+          {
+            x: ((this._fixedHandler as unknown as Segment).attribute.points[0] as Point).x,
+            y: ((this._fixedHandler as unknown as Segment).attribute.points[0] as Point).y
+          }
+        ],
+        [attrKey]: attrValue
+      });
+    } else {
+      (this._currentHandler as unknown as Segment).setAttributes({
+        x: closestPoint.x,
+        y: closestPoint.y
+      });
 
-    this._overlayLine.setAttributes({
-      points: [
-        closestPoint,
-        {
-          x: (this._fixedHandler.attribute.points[0] as Point).x,
-          y: (this._fixedHandler.attribute.points[0] as Point).y
-        }
-      ]
-    });
+      // TODO: 优化，可以缓存
+      this._overlayLine.setAttributes({
+        points: getInsertPoints(
+          {
+            x: this._overlayStartHandler.attribute.x,
+            y: this._overlayStartHandler.attribute.y
+          },
+          {
+            x: this._overlayEndHandler.attribute.x,
+            y: this._overlayEndHandler.attribute.y
+          },
+          (this._model.getSpec() as IStepMarkLineSpec).connectDirection,
+          30 + this._getMaxLengthBetweenHandlers()
+        )
+      });
+    }
   };
 
   private _onHandlerDragEnd = (e: any) => {
@@ -350,16 +450,68 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
 
     // 2. 更新当前 markLine 组件
     // Important: 拖拽结束，恢复 markLine 的交互
-    this._element.setAttributes({
-      pickable: true,
-      childrenPickable: true,
-      // @ts-ignore
-      points: [this._overlayStartHandler.attribute.points[0], this._overlayEndHandler.attribute.points[0]],
-      label: {
-        text: labelText
-      }
-    });
+    const offset = this._getMaxLengthBetweenHandlers();
 
+    if (this._element.name === 'growthMarkLine') {
+      let attrKey;
+      let attrValue;
+      if (this._model.getRelativeSeries().direction === 'horizontal') {
+        attrKey = 'dx';
+        attrValue = offset + 30;
+      } else {
+        attrKey = 'dy';
+        attrValue = -offset - 30;
+      }
+      this._element.setAttributes({
+        pickable: true,
+        childrenPickable: true,
+        // @ts-ignore
+        points: [
+          (this._overlayStartHandler as unknown as Segment).attribute.points[0],
+          (this._overlayEndHandler as unknown as Segment).attribute.points[0]
+        ],
+        label: {
+          text: labelText
+        },
+        [attrKey]: attrValue
+      });
+      newMarkLineSpec[attrKey === 'dx' ? 'offsetX' : 'offsetY'] = attrValue;
+    } else {
+      this._element.setAttributes({
+        pickable: true,
+        childrenPickable: true,
+        // @ts-ignore
+        points: getInsertPoints(
+          {
+            x: this._overlayStartHandler.attribute.x,
+            y: this._overlayStartHandler.attribute.y
+          },
+          {
+            x: this._overlayEndHandler.attribute.x,
+            y: this._overlayEndHandler.attribute.y
+          },
+          (this._model.getSpec() as IStepMarkLineSpec).connectDirection,
+          30 + offset
+        ),
+        label: {
+          text: labelText,
+          ...getTextOffset(
+            {
+              x: this._overlayStartHandler.attribute.x,
+              y: this._overlayStartHandler.attribute.y
+            },
+            {
+              x: this._overlayEndHandler.attribute.x,
+              y: this._overlayEndHandler.attribute.y
+            },
+            (this._model.getSpec() as IStepMarkLineSpec).connectDirection,
+            30 + offset
+          )
+        }
+      });
+      newMarkLineSpec.expandDistance = 30 + offset;
+    }
+    // console.log(newMarkLineSpec);
     vglobal.removeEventListener('pointermove', this._onHandlerDrag);
     vglobal.removeEventListener('pointerup', this._onHandlerDragEnd);
   };
@@ -386,7 +538,7 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
       return this._dataPoints;
     }
     const model = this._model as MarkLine;
-    const series = model.getRelativeSeries() as ISeries;
+    const series = model.getRelativeSeries() as ICartesianSeries;
     const region = series.getRegion();
     const { x: regionStartX, y: regionStartY } = region.getLayoutStartPoint();
     // TODO: 需要根据不同的系列名称获取不同的图形节点
@@ -408,11 +560,26 @@ export class GrowthMarkLineEditor extends BaseEditorElement {
             (series.direction === 'horizontal'
               ? graphItem.attribute.y + graphItem.attribute.height / 2
               : graphItem.attribute.y) + regionStartY,
-          data: array(element.data)[0]
+          data: array(element.data)[0],
+          length: series.direction === 'horizontal' ? graphItem.attribute.width : graphItem.attribute.height
         };
       });
 
     this._dataPoints = dataPoints;
     return dataPoints;
+  }
+
+  private _getMaxLengthBetweenHandlers() {
+    const startDataIndex = this._overlayStartHandler.data[DEFAULT_DATA_KEY];
+    const endDataIndex = this._overlayEndHandler.data[DEFAULT_DATA_KEY];
+    const maxLengthData = this._dataPoints
+      .slice(Math.min(startDataIndex, endDataIndex), Math.max(startDataIndex, endDataIndex) + 1)
+      .sort((prev: DataPoint, next: DataPoint) => next.length - prev.length)[0];
+
+    const offset =
+      this._model.getRelativeSeries().direction === 'horizontal'
+        ? maxLengthData.x - Math.max(this._dataPoints[startDataIndex].x, this._dataPoints[endDataIndex].x)
+        : maxLengthData.y - Math.min(this._dataPoints[startDataIndex].y, this._dataPoints[endDataIndex].y);
+    return Math.abs(offset);
   }
 }
--- packages/vchart-editor/src/elements/chart/utils/marker.ts
+++ packages/vchart-editor/src/elements/chart/utils/marker.ts
@@ -2,6 +2,10 @@
  * @description 获取标注的默认初始配置
  */
 import { IChartSpec, IVChart } from '@visactor/vchart';
+import { IPointLike } from '@visactor/vutils';
+
+// TODO: 不同的标注需要给不同的 zIndex
+// TODO: Marker 组件的 group 需要关闭 pickable
 
 type Datum = {
   [key: string]: any;
@@ -60,4 +64,105 @@ export function getDefaultGrowthMarkLineConfig(chart: IVChart) {
     interactive: true,
     name: 'growthMarkLine'
   };
+}
+
+// TODO: 移到 vutils-extension 中
+export function getInsertPoints(
+  start: IPointLike,
+  end: IPointLike,
+  direction: 'top' | 'bottom' | 'left' | 'right',
+  offset: number = 0
+) {
+  const points: IPointLike[] = [];
+  const dy = start.y - end.y;
+  const dx = start.x - end.x;
+
+  switch (direction) {
+    case 'top':
+      points.push(start);
+      points.push({
+        x: start.x,
+        y: dy > 0 ? start.y - offset - Math.abs(dy) : start.y - offset
+      });
+      points.push({
+        x: end.x,
+        y: dy > 0 ? end.y - offset : end.y - offset - Math.abs(dy)
+      });
+      points.push(end);
+      break;
+    case 'bottom':
+      points.push(start);
+      points.push({ x: start.x, y: dy < 0 ? start.y + offset + Math.abs(dy) : start.y + offset });
+      points.push({ x: end.x, y: dy < 0 ? end.y + offset : end.y + offset + Math.abs(dy) });
+      points.push(end);
+      break;
+    case 'left':
+      points.push(start);
+      points.push({
+        x: dx > 0 ? start.x - offset - Math.abs(dx) : start.x - offset,
+        y: start.y
+      });
+      points.push({
+        x: dx > 0 ? end.x - offset : end.x - offset - Math.abs(dx),
+        y: end.y
+      });
+      points.push(end);
+      break;
+    case 'right':
+      points.push(start);
+      points.push({
+        x: dx > 0 ? start.x + offset : start.x + offset + Math.abs(dx),
+        y: start.y
+      });
+      points.push({
+        x: dx > 0 ? end.x + offset + Math.abs(dx) : end.x + offset,
+        y: end.y
+      });
+      points.push(end);
+      break;
+    default:
+      break;
+  }
+  return points;
+}
+
+// TODO: 移到 vutils-extension 中
+export function getTextOffset(
+  start: IPointLike,
+  end: IPointLike,
+  direction: 'top' | 'bottom' | 'left' | 'right',
+  offset: number = 0
+) {
+  const dy = start.y - end.y;
+  const dx = start.x - end.x;
+
+  if (direction === 'bottom') {
+    return {
+      dx: dx > 0 ? -dx / 2 : Math.abs(dx / 2),
+      dy: dy > 0 ? offset : Math.abs(dy) + offset
+    };
+  }
+
+  if (direction === 'top') {
+    return {
+      dx: dx > 0 ? -Math.abs(dx / 2) : +Math.abs(dx / 2),
+      dy: dy > 0 ? -(Math.abs(dy) + offset) : -offset
+    };
+  }
+
+  if (direction === 'left') {
+    return {
+      dx: dx > 0 ? -dx - offset : -offset,
+      dy: dy > 0 ? -(dy / 2) : Math.abs(dy / 2)
+    };
+  }
+
+  if (direction === 'right') {
+    return {
+      dx: dx > 0 ? offset : Math.abs(dx) + offset,
+      dy: dy > 0 ? -(dy / 2) : Math.abs(dy / 2)
+    };
+  }
+
+  return {};
 }
