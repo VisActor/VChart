68acba306b88cd4aca0c051b3ab6992bc03daf8c
kkxxkk2019 <just_star@qq.com>
2023-10-16 09:55:30 +0800

    feat(vchart-editor): add the interaction of total diff marker line

--- packages/vchart-editor/src/elements/chart/template/templates/bar.ts
+++ packages/vchart-editor/src/elements/chart/template/templates/bar.ts
@@ -136,75 +136,264 @@ const spec = {
   },
 
   // TODO: 变成默认的配置
-  // 均值线
-  // markLine: [
-  //   {
-  //     name: 'avgMarkLineVertical',
-  //     interactive: true,
-  //     y: 'average',
-  //     endSymbol: {
-  //       visible: true,
-  //       size: 12,
-  //       refX: 6,
-  //       symbolType: 'triangleLeft',
-  //       autoRotate: false
-  //     },
-  //     label: {
-  //       visible: true,
-  //       autoRotate: false,
-  //       formatMethod: markData => {
-  //         return parseInt(markData[0].y, 10);
-  //       },
-  //       position: 'end',
-  //       labelBackground: {
-  //         visible: false
-  //       },
-  //       style: {
-  //         fill: '#000'
-  //       },
-  //       refX: 12,
-  //       refY: 0
-  //     },
-  //     line: {
-  //       style: {
-  //         stroke: '#000'
-  //       }
-  //     }
-  //   },
-  //   {
-  //     name: 'avgMarkLineHorizontal',
-  //     interactive: true,
-  //     x: 'VT',
-  //     endSymbol: {
-  //       visible: true,
-  //       size: 12,
-  //       refX: 6,
-  //       symbolType: 'triangleDown',
-  //       autoRotate: false
-  //     },
-  //     label: {
-  //       visible: true,
-  //       autoRotate: false,
-  //       text: 'VT',
-  //       position: 'end',
-  //       labelBackground: {
-  //         visible: false
-  //       },
-  //       style: {
-  //         fill: '#000',
-  //         textAlign: 'center',
-  //         textBaseline: 'bottom'
-  //       },
-  //       refX: 12,
-  //       refY: 0
-  //     },
-  //     line: {
-  //       style: {
-  //         stroke: '#000'
-  //       }
-  //     }
-  //   }
-  // ]
+  markLine: [
+    // // 均值线
+    // {
+    //   name: 'avgMarkLineVertical',
+    //   interactive: true,
+    //   y: 'average',
+    //   endSymbol: {
+    //     visible: true,
+    //     size: 12,
+    //     refX: 6,
+    //     symbolType: 'triangleLeft',
+    //     autoRotate: false
+    //   },
+    //   label: {
+    //     visible: true,
+    //     autoRotate: false,
+    //     formatMethod: markData => {
+    //       return parseInt(markData[0].y, 10);
+    //     },
+    //     position: 'end',
+    //     labelBackground: {
+    //       visible: false
+    //     },
+    //     style: {
+    //       fill: '#000'
+    //     },
+    //     refX: 12,
+    //     refY: 0
+    //   },
+    //   line: {
+    //     style: {
+    //       stroke: '#000'
+    //     }
+    //   }
+    // },
+    // {
+    //   name: 'avgMarkLineHorizontal',
+    //   interactive: true,
+    //   x: 'VT',
+    //   endSymbol: {
+    //     visible: true,
+    //     size: 12,
+    //     refX: 6,
+    //     symbolType: 'triangleDown',
+    //     autoRotate: false
+    //   },
+    //   label: {
+    //     visible: true,
+    //     autoRotate: false,
+    //     text: 'VT',
+    //     position: 'end',
+    //     labelBackground: {
+    //       visible: false
+    //     },
+    //     style: {
+    //       fill: '#000',
+    //       textAlign: 'center',
+    //       textBaseline: 'bottom'
+    //     },
+    //     refX: 12,
+    //     refY: 0
+    //   },
+    //   line: {
+    //     style: {
+    //       stroke: '#000'
+    //     }
+    //   }
+    // },
+    // // 复合增长箭头，默认为第一维度和最后一个维度的值差异，TODO：需要根据图表的spec 来获取初始 coordinates
+    // {
+    //   coordinates: [
+    //     {
+    //       State: 'WY',
+    //       Age: 'Under 5 Years',
+    //       Population: 25635
+    //     },
+    //     {
+    //       State: 'AK',
+    //       Age: 'Under 5 Years',
+    //       Population: 72083
+    //     }
+    //   ],
+    //   line: {
+    //     style: {
+    //       lineDash: [0],
+    //       lineWidth: 2,
+    //       stroke: '#000'
+    //     }
+    //   },
+    //   label: {
+    //     position: 'middle',
+    //     text: (((22153 + 85640 + 72083 - 25635 + 1890 + 9314) / (25635 + 1890 + 9314)) * 100).toFixed(0) + '%',
+    //     labelBackground: {
+    //       style: {
+    //         fill: '#fff',
+    //         fillOpacity: 1,
+    //         stroke: '#000',
+    //         lineWidth: 1,
+    //         cornerRadius: 4
+    //       }
+    //     },
+    //     style: {
+    //       fill: '#000'
+    //     }
+    //   },
+    //   endSymbol: {
+    //     size: 12,
+    //     refX: -6
+    //   },
+    //   offsetY: -50,
+    //   interactive: true,
+    //   name: 'growthMarkLine'
+    // },
+    // 总计差异
+    {
+      type: 'type-step',
+      coordinates: [
+        {
+          State: 'WY',
+          Age: 'Under 5 Years',
+          Population: 25635
+        },
+        {
+          State: 'AK',
+          Age: 'Under 5 Years',
+          Population: 72083
+        }
+      ],
+      connectDirection: 'top',
+      expandDistance: 30,
+      line: {
+        style: {
+          lineDash: [0],
+          lineWidth: 2,
+          stroke: '#000',
+          cornerRadius: 4
+        }
+      },
+      label: {
+        position: 'middle',
+        text: 'ssss',
+        labelBackground: {
+          style: {
+            fill: '#fff',
+            fillOpacity: 1,
+            stroke: '#000',
+            lineWidth: 1,
+            cornerRadius: 4
+          }
+        },
+        style: {
+          fill: '#000'
+        }
+      },
+      endSymbol: {
+        size: 12,
+        refX: -6
+      },
+      interactive: true,
+      name: 'totalDiffMarkLine'
+    },
+    {
+      line: {
+        style: {
+          lineDash: [0],
+          stroke: '#000',
+          lineWidth: 2,
+          cornerRadius: 4
+        }
+      },
+      startSymbol: {
+        visible: false,
+        symbolType: 'triangle',
+        size: 10,
+        style: {
+          stroke: null,
+          lineWidth: 0,
+          fill: 'rgba(46, 47, 50)'
+        }
+      },
+      endSymbol: {
+        visible: true,
+        symbolType: 'triangle',
+        size: 12,
+        style: {
+          stroke: null,
+          lineWidth: 0,
+          fill: 'rgba(46, 47, 50)'
+        },
+        refX: -6
+      },
+      label: {
+        refY: 5,
+        style: {
+          fontSize: 14,
+          fontWeight: 'normal',
+          fontStyle: 'normal',
+          fill: '#000',
+          stroke: '#ffffff',
+          lineWidth: 0
+        },
+        labelBackground: {
+          padding: {
+            top: 2,
+            bottom: 2,
+            right: 4,
+            left: 4
+          },
+          style: {
+            cornerRadius: 4,
+            fill: '#fff',
+            fillOpacity: 1,
+            stroke: '#000',
+            lineWidth: 1
+          }
+        },
+        position: 'middle',
+        text: '-93%'
+      },
+      type: 'type-step',
+      coordinates: [
+        {
+          State: 'WY',
+          Age: 'Under 5 Years',
+          Population: 25635,
+          __VCHART_DEFAULT_DATA_INDEX: 0,
+          __VCHART_DEFAULT_DATA_KEY: 0,
+          __VCHART_STACK_END: 25635,
+          __VCHART_STACK_START: 0,
+          __VCHART_STACK_START_PERCENT: 0,
+          __VCHART_STACK_END_PERCENT: 1,
+          __VCHART_STACK_TOTAL: 25635,
+          __VCHART_STACK_TOTAL_PERCENT: 1,
+          __VCHART_STACK_TOTAL_TOP: true,
+          VGRAMMAR_DATA_ID_KEY_17: 0
+        },
+        {
+          State: 'WY',
+          Age: '5 to 13 Years',
+          Population: 1890,
+          __VCHART_DEFAULT_DATA_INDEX: 1,
+          __VCHART_DEFAULT_DATA_KEY: 1,
+          __VCHART_STACK_END: 1890,
+          __VCHART_STACK_START: 0,
+          __VCHART_STACK_START_PERCENT: 0,
+          __VCHART_STACK_END_PERCENT: 1,
+          __VCHART_STACK_TOTAL: 1890,
+          __VCHART_STACK_TOTAL_PERCENT: 1,
+          __VCHART_STACK_TOTAL_TOP: true,
+          VGRAMMAR_DATA_ID_KEY_17: 1
+        }
+      ],
+      connectDirection: 'top',
+      expandDistance: 30,
+      interactive: true,
+      name: 'totalDiffMarkLine'
+    }
+  ]
   // 区域标注
   // markArea: [
   //   {
@@ -254,53 +443,6 @@ const spec = {
   //     }
   //   }
   // ],
-
-  // 复合增长箭头，默认为第一维度和最后一个维度的值差异，TODO：需要根据图表的spec 来获取初始 coordinates
-  markLine: {
-    coordinates: [
-      {
-        State: 'WY',
-        Age: 'Under 5 Years',
-        Population: 25635
-      },
-      {
-        State: 'AK',
-        Age: '14 to 17 Years',
-        // Population: 22153 + 85640 + 72083
-        Population: 22153
-      }
-    ],
-    line: {
-      style: {
-        lineDash: [0],
-        lineWidth: 2,
-        stroke: '#000'
-      }
-    },
-    label: {
-      position: 'middle',
-      text: (((22153 + 85640 + 72083 - 25635 + 1890 + 9314) / (25635 + 1890 + 9314)) * 100).toFixed(0) + '%',
-      labelBackground: {
-        style: {
-          fill: '#fff',
-          fillOpacity: 1,
-          stroke: '#000',
-          lineWidth: 1,
-          cornerRadius: 4
-        }
-      },
-      style: {
-        fill: '#000'
-      }
-    },
-    endSymbol: {
-      size: 12,
-      refX: -6
-    },
-    offsetY: -30,
-    interactive: true,
-    name: 'growthMarkLine'
-  }
 };
 
 export class BarTemp extends BaseTemp {
