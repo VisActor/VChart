91fe3c0b800fbb6dfc3ad5a21fc7324e84390a8a
Howard Zhang <zamhown@live.com>
2023-10-18 11:49:37 +0800

    fix: on-demand merge default theme

--- packages/vchart/src/theme/builtin/index.ts
+++ packages/vchart/src/theme/builtin/index.ts
@@ -8,15 +8,19 @@ import { darkTheme } from './dark';
 import { lightTheme } from './light';
 
 /** 声明内置主题 */
-export const builtinThemes: [string, ITheme][] = [
-  [lightTheme.name, lightTheme],
-  [darkTheme.name, darkTheme]
-];
+export const builtinThemes: Record<string, ITheme> = {
+  [lightTheme.name]: lightTheme,
+  [darkTheme.name]: darkTheme
+};
 /** 声明默认主题 */
 export const defaultThemeName = lightTheme.name;
 
-/** 全局主题 map (包含用户新注册的主题，没有 merge 过默认主题的原始主题) */
-export const themes: Map<string, ITheme> = new Map(builtinThemes);
+/** 全局主题 map (包含用户新注册的主题) */
+export const themes: Map<string, ITheme> = new Map(Object.keys(builtinThemes).map(key => [key, builtinThemes[key]]));
+/** 主题 map 中的元素是否 merge 过默认主题 (非默认主题的其他内置主题没有 merge 过默认主题) */
+export const hasThemeMerged: Map<string, boolean> = new Map(
+  Object.keys(builtinThemes).map(key => [key, key === defaultThemeName])
+);
 
 /** 使新主题基于默认主题扩展，保证基础值 */
 export const getMergedTheme = (theme: Partial<ITheme>): ITheme => mergeTheme({}, themes.get(defaultThemeName), theme);
