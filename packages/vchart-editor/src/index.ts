ed957f82f271f3d8624ceec1f6c3039ce8136522
purpose233 <zhangsu.melon@gmail.com>
2023-10-23 16:40:28 +0800

    feat: register default parser

--- packages/vchart-editor/src/index.ts
+++ packages/vchart-editor/src/index.ts
@@ -16,10 +16,14 @@ export * from './elements/chart/template/templates/baseTemp';
 
 import { EditorFactory } from './core/factory';
 import { ClipBoardParser } from './elements/chart/data/parser/clipboard';
+import { CSVParser } from './elements/chart/data/parser/csv';
+import { StandardParser } from './elements/chart/data/parser/standard';
 import { BarTemp } from './elements/chart/template/templates/bar';
 import { LineTemp } from './elements/chart/template/templates/line';
 
 EditorFactory.registerTemp('bar', BarTemp);
 EditorFactory.registerTemp('line', LineTemp);
 
+EditorFactory.registerParser('csv', CSVParser);
+EditorFactory.registerParser('standard', StandardParser);
 EditorFactory.registerParser('clipBoard', ClipBoardParser);
