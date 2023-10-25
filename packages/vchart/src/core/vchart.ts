476ebc28a062827a8feabcdf63fb2fcadf216486
skie1997 <skie1997@outlook.com>
2023-10-25 17:14:57 +0800

    fix: slove event off error after release

--- packages/vchart/src/core/vchart.ts
+++ packages/vchart/src/core/vchart.ts
@@ -998,18 +998,18 @@ export class VChart implements IVChart {
   }
   off(eType: string, handler?: EventCallback<EventParams>): void {
     if (handler) {
-      const index = this._userEvents.findIndex(e => e.eType === eType && e.handler === handler);
+      const index = this._userEvents?.findIndex(e => e.eType === eType && e.handler === handler);
       if (index >= 0) {
         this._userEvents.splice(index, 1);
         this._event?.off(eType, handler);
       }
     } else {
-      this._userEvents.forEach(e => {
+      this._userEvents?.forEach(e => {
         if (e.eType === eType) {
           this._event?.off(eType, e.handler);
         }
       });
-      this._userEvents = this._userEvents.filter(e => e.eType !== eType);
+      this._userEvents = this._userEvents?.filter(e => e.eType !== eType);
     }
   }
 
