6fe010e8d6ef19e6809fb65e2db895ac39258e17
Howard Zhang <zamhown@live.com>
2023-10-25 16:38:41 +0800

    fix: update unit test

--- packages/vchart/__tests__/unit/chart/histogram.test.ts
+++ packages/vchart/__tests__/unit/chart/histogram.test.ts
@@ -89,7 +89,7 @@ describe('histogram chart test', () => {
     });
 
     // mark
-    expect(series.getMarks().length).toEqual(3);
+    expect(series.getMarks().length).toEqual(2);
 
     expect(chart.getRegionsInIndex().length).toEqual(1);
     expect(series.getRegion().id).toEqual(chart.getRegionsInIndex()[0].id);
