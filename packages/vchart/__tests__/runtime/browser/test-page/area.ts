97d57b0752e4e59b71835a49c851139dd4887e5b
Howard Zhang <zamhown@live.com>
2023-10-26 22:14:03 +0800

    feat: `updateElement` callback in tooltip

--- packages/vchart/__tests__/runtime/browser/test-page/area.ts
+++ packages/vchart/__tests__/runtime/browser/test-page/area.ts
@@ -97,7 +97,10 @@ const getSpec = (): IAreaChartSpec => ({
     },
     updateElement: el => {
       el.style.width = 'auto';
+      el.style.height = 'auto';
+      el.style.minHeight = 'auto';
       if (el.lastElementChild?.id !== 'test') {
+        el.innerHTML = '';
         const div = document.createElement('div');
         div.id = 'test';
         div.style.width = '200px';
