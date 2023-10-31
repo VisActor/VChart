a1821bfb6d88d474c1d6cf7f4b08f76b91ff498b
Howard Zhang <zamhown@live.com>
2023-10-31 16:55:43 +0800

    chore: update config of vchart-types

--- packages/vchart-types/util/math.d.ts
+++ packages/vchart-types/util/math.d.ts
@@ -6,12 +6,9 @@ export declare const isClose: typeof isNumberClose;
 export { isGreater, isLess, normalizeAngle, angleLabelOrientAttribute };
 export declare function polarToCartesian(point: IPolarPoint): IPoint;
 export declare function computeQuadrant(angle: number): Quadrant;
-export declare function normalizeStartEndAngle(
-  start: number | null,
-  end: number | null
-): {
-  startAngle: number;
-  endAngle: number;
+export declare function normalizeStartEndAngle(start: number | null, end: number | null): {
+    startAngle: number;
+    endAngle: number;
 };
 export declare function outOfBounds(bounds: IBoundsLike, x: number, y: number): boolean;
 export declare function min(data: any[], field?: string): number;
@@ -23,8 +20,8 @@ export declare function standardDeviation(data: any[], field?: string): number;
 export declare function median(data: any[], field?: string): number;
 export declare function regression(data: any[], fieldX?: string, fieldY?: string): any[];
 export declare function radiusLabelOrientAttribute(angle: number): {
-  align: 'left' | 'right' | 'center';
-  baseline: 'top' | 'middle' | 'bottom';
+    align: "left" | "right" | "center";
+    baseline: "top" | "bottom" | "middle";
 };
 export declare function vectorAngle(v1: IPoint, v2: IPoint): number;
 export declare function distance(p1: IPoint, p2?: IPoint): number;
