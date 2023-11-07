8a5ffc719e0611fbde1007983e048ac137666c70
Howard Zhang <zamhown@live.com>
2023-11-07 17:38:17 +0800

    feat: update theme description

--- packages/vchart-theme/src/semi-design/dark/index.ts
+++ packages/vchart-theme/src/semi-design/dark/index.ts
@@ -4,6 +4,7 @@ import { SEMI_FONT_FAMILY } from '../common/constants';
 
 export const semiDesignDark: ITheme = {
   name: 'semiDesignDark',
+  description: 'Semi Design - dark',
   type: 'dark',
   fontFamily: SEMI_FONT_FAMILY,
   colorScheme
