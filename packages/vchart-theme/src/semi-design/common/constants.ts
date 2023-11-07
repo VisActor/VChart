08ae13e5bd5c8046af08d3c5272742412dbb6d87
Howard Zhang <zamhown@live.com>
2023-11-07 17:27:18 +0800

    fix: update semi theme

---
+++ packages/vchart-theme/src/semi-design/common/constants.ts
@@ -0,0 +1,2 @@
+export const SEMI_FONT_FAMILY =
+  'Inter,-apple-system,BlinkMacSystemFont,Segoe UI,PingFang SC,Hiragino Sans GB,Microsoft YaHei,Helvetica Neue,Helvetica,Arial,sans-serif';
--- packages/vchart-theme/src/semi-design/light/index.ts
+++ packages/vchart-theme/src/semi-design/light/index.ts
@@ -1,8 +1,10 @@
 import { ITheme } from '@visactor/vchart-types';
 import { colorScheme } from './color-scheme';
+import { SEMI_FONT_FAMILY } from '../common/constants';
 
 export const semiDesignLight: ITheme = {
   name: 'semiDesignLight',
   type: 'light',
+  fontFamily: SEMI_FONT_FAMILY,
   colorScheme
 };
