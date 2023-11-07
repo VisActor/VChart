4e5913790ed4cc28e1eb8b684232df0ab1dca8aa
Howard Zhang <zamhown@live.com>
2023-11-07 17:38:17 +0800

    feat: update theme description

--- packages/vchart-theme/src/semi-design/light/index.ts
+++ packages/vchart-theme/src/semi-design/light/index.ts
@@ -4,6 +4,7 @@ import { SEMI_FONT_FAMILY } from '../common/constants';
 
 export const semiDesignLight: ITheme = {
   name: 'semiDesignLight',
+  description: 'Semi Design - light',
   type: 'light',
   fontFamily: SEMI_FONT_FAMILY,
   colorScheme
