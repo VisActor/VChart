b83cdb6f077e4da692210af4745f77cf1de87b4c
Howard Zhang <zamhown@live.com>
2023-11-07 17:36:55 +0800

    feat: update theme description

--- packages/vchart-theme/src/v-screen/index.ts
+++ packages/vchart-theme/src/v-screen/index.ts
@@ -4,7 +4,7 @@ import { getVScreenCommonTheme } from './common';
 export const vScreenThemeList = Object.keys(vScreenColorScheme).map(name =>
   getVScreenCommonTheme(
     `vScreen${name[0].toUpperCase()}${name.slice(1)}`,
-    vScreenColorScheme[name].name,
+    `大屏-${vScreenColorScheme[name].name}`,
     vScreenColorScheme[name].colors
   )
 );
