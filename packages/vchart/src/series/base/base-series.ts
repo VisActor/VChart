c87abbf19adcf1cba13fcc7690e1c11c68ad1ecf
lixuefei.1313 <lixuef1313@163.com>
2023-11-27 14:34:14 +0800

    feat: change api in series to get data

--- packages/vchart/src/series/base/base-series.ts
+++ packages/vchart/src/series/base/base-series.ts
@@ -345,7 +345,7 @@ export abstract class BaseSeries<T extends ISeriesSpec> extends BaseModel<T> imp
 
   /** data */
   protected initData(): void {
-    const d = this._spec.data ?? this._option.getChart().getSeriesData(this._spec.dataId, this._spec.dataIndex);
+    const d = this._spec.data ?? this._option.getSeriesData(this._spec.dataId, this._spec.dataIndex);
     if (d) {
       this._rawData = dataToDataView(d, this._dataSet, this._option.sourceDataList, {
         onError: this._option?.onError
