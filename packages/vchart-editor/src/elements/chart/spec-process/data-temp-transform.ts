7f3acf24a95b8ad7daa8ddd5489ebe81ca70368f
lixuefei.1313 <lixuef1313@163.com>
2023-12-01 14:29:33 +0800

    feat: fix bug in dataTemp update

--- packages/vchart-editor/src/elements/chart/spec-process/data-temp-transform.ts
+++ packages/vchart-editor/src/elements/chart/spec-process/data-temp-transform.ts
@@ -67,7 +67,9 @@ export class DataTempTransform {
     } else if (this._changeType === 'dataTemp') {
       if (this._checkEnable(this._nextDataParser, this._nextTemp)) {
         // 数据 + 模版更新成功
-        this._chart.clearDataForChartTypeChange({ clearCurrent: true });
+        if (this._specTemp) {
+          this._chart.clearDataForChartTypeChange({ clearCurrent: true });
+        }
         this._setTempToNext();
         this._dataChangeSuccess(d, this._changeType);
       }
