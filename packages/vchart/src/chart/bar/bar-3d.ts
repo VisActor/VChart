232ecc419dcae20ca78a845d630b079d06542995
zhouxinyu <zhouxinyu12138@gmail.com>
2023-12-05 14:33:44 +0800

    fix: fix 3d bar chart with seriesField issue, closed #1646

--- packages/vchart/src/chart/bar/bar-3d.ts
+++ packages/vchart/src/chart/bar/bar-3d.ts
@@ -4,12 +4,14 @@ import { CartesianChart } from '../cartesian/cartesian';
 import { ChartTypeEnum } from '../interface/type';
 import type { IBar3dChartSpec } from './interface';
 import { Factory } from '../../core/factory';
+import { BarChart } from './bar';
 
-export class Bar3dChart extends CartesianChart {
+export class Bar3dChart extends BarChart {
   static readonly type: string = ChartTypeEnum.bar3d;
   static readonly view: string = 'singleDefault';
   readonly type: string = ChartTypeEnum.bar3d;
   readonly seriesType: string = SeriesTypeEnum.bar3d;
+  protected _canStack: boolean = true;
 
   protected _getDefaultSeriesSpec(spec: any): any {
     return {
