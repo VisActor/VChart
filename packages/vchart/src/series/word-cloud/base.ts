652b527b56a37e86a15294be739ab9b311fc8c2c
xiaoluoHe <sevnape@outlook.com>
2023-12-12 18:45:15 +0800

    refactor: wordcloud_3d_transform_config

--- packages/vchart/src/series/word-cloud/base.ts
+++ packages/vchart/src/series/word-cloud/base.ts
@@ -64,6 +64,7 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
   protected _colorList: string[];
   protected _rotateAngles?: number[];
   protected _fontWeightRange?: [number, number];
+  protected _textField?: string;
 
   protected _fontSizeRange?: [number, number] = [DEFAULT_MIN_FONT_SIZE, DEFAULT_MIN_FONT_SIZE];
   setFontSizeRange(fontSizeRange: [number, number]) {
@@ -110,8 +111,8 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
     this._maskShape = this._spec.maskShape ?? DEFAULT_MASK_SHAPE;
     this._keepAspect = this._spec.keepAspect;
     this._random = this._spec.random ?? DEFAULT_RANDOM;
-    this._fontPadding = this._spec?.word?.padding ?? this._theme?.wordCloud?.word?.padding ?? DEFAULT_FONT_PADDING;
-
+    this._fontPadding = this._spec.word?.padding ?? DEFAULT_FONT_PADDING;
+    this._textField = this._spec.word?.formatMethod ? WORD_CLOUD_TEXT : this._nameField;
     // 普通词云spec相关
     this._wordCloudConfig = {
       drawOutOfBound: DEFAULT_DRAW_OUT_OF_BOUND,
@@ -125,10 +126,9 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
       fillingSeriesField: this.getSeriesField(),
       fillingRotateAngles: DEFAULT_ROTATE_ANGLES,
       layoutMode: 'default',
-      ...this._spec?.wordCloudShapeConfig
+      ...this._spec.wordCloudShapeConfig
     };
-    this._fillingFontPadding =
-      this._spec?.fillingWord?.padding ?? this._theme?.wordCloud?.fillingWord?.padding ?? DEFAULT_FONT_PADDING;
+    this._fillingFontPadding = this._spec.fillingWord?.padding ?? DEFAULT_FONT_PADDING;
 
     this._isWordCloudShape = !SHAPE_TYPE.includes(this._maskShape);
     this._defaultFontFamily = this._option.getTheme().fontFamily;
@@ -150,7 +150,7 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
   initMarkStyle() {
     const wordMark = this._wordMark;
     const fillingWordMark = this._fillingWordMark;
-    const textField = this._spec.word?.formatMethod ? WORD_CLOUD_TEXT : this._nameField;
+    const wordSpec = this._spec.word ?? {};
     if (wordMark) {
       this.setMarkStyle(
         wordMark,
@@ -158,7 +158,7 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
           fill: this._colorHexField
             ? (datum: Datum) => datum[this._colorHexField]
             : this.getWordColorAttribute(this._seriesField, false),
-          text: (datum: Datum) => datum[textField],
+          text: (datum: Datum) => datum[this._textField],
           x: (datum: Datum) => datum.x,
           y: (datum: Datum) => datum.y,
           fontFamily: (datum: Datum) => datum.fontFamily,
@@ -174,7 +174,7 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
       this.setMarkStyle(
         wordMark,
         {
-          fontFamily: this._spec.word?.style?.fontFamily ?? this._defaultFontFamily
+          fontFamily: wordSpec.style?.fontFamily ?? this._defaultFontFamily
         },
         'normal',
         AttributeLevel.User_Mark
@@ -187,7 +187,7 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
           fill: this._wordCloudShapeConfig.fillingColorHexField
             ? (datum: Datum) => datum[this._wordCloudShapeConfig.fillingColorHexField]
             : this.getWordColorAttribute(this._wordCloudShapeConfig.fillingSeriesField, true),
-          text: (datum: Datum) => datum[textField],
+          text: (datum: Datum) => datum[this._textField],
           x: (datum: Datum) => datum.x,
           y: (datum: Datum) => datum.y,
           fontFamily: (datum: Datum) => datum.fontFamily,
@@ -204,7 +204,7 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
       this.setMarkStyle(
         fillingWordMark,
         {
-          fontFamily: this._spec.word?.style?.fontFamily ?? this._defaultFontFamily
+          fontFamily: wordSpec.style?.fontFamily ?? this._defaultFontFamily
         },
         'normal',
         AttributeLevel.User_Mark
@@ -281,7 +281,6 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
     const valueScale = new LinearScale();
     const fontWeightRange = this._fontWeightRange;
     const rotateAngles = this._rotateAngles;
-    const fontWeightField = this._fontWeightField;
     const fillingRotateAngles = this._wordCloudShapeConfig.fillingRotateAngles;
 
     // fontWeight处理
@@ -315,43 +314,22 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
         return fillingRotateAngles[Math.floor(Math.random() * fillingRotateAngles.length)];
       }
     });
+    const wordSpec = this._spec.word ?? {};
 
     // text fromat method 处理
-    if (this._spec.word?.formatMethod) {
+    if (wordSpec.formatMethod) {
       wordCloudTransforms.push({
         type: 'map',
         as: WORD_CLOUD_TEXT,
-        callback: this._spec.word.formatMethod
+        callback: wordSpec.formatMethod
       });
     }
-
-    const textField = this._spec.word?.formatMethod ? WORD_CLOUD_TEXT : this._nameField;
 
     // 词云 transform
     if (!this._isWordCloudShape) {
       wordCloudTransforms.push({
         type: 'wordcloud',
-        // TIP: 非浏览器环境下，使用 fast 布局，否则会出现兼容问题
-        layoutType: !isTrueBrowser(this._option.mode) ? 'fast' : this._wordCloudConfig.layoutMode,
-        size: [width, height],
-        shape: this._maskShape,
-        dataIndexKey: DEFAULT_DATA_KEY,
-        text: { field: textField },
-        fontSize: valueField ? { field: valueField } : this._fontSizeRange[0],
-        fontSizeRange: this._fontSizeRange,
-        padding: this._fontPadding,
-        rotate: { field: WORD_CLOUD_ANGLE },
-        fontFamily: this._fontFamilyField ?? this._spec.word?.style?.fontFamily ?? this._defaultFontFamily,
-        fontWeight: fontWeightField ? { field: fontWeightField } : valueField ? { field: WORD_CLOUD_WEIGHT } : null,
-        fontStyle: this._fontStyleField ?? this._spec.word?.style?.fontStyle,
-
-        randomVisible: this._random,
-        clip: this._wordCloudConfig.drawOutOfBound === 'clip',
-        shrink: this._wordCloudConfig.zoomToFit.shrink,
-        enlarge: this._wordCloudConfig.zoomToFit.enlarge,
-        minFontSize: this._wordCloudConfig.zoomToFit.fontSizeLimitMin,
-        progressiveTime: this._wordCloudConfig.progressiveTime,
-        progressiveStep: this._wordCloudConfig.progressiveStep
+        ...this._wordCloudTransformOption()
       });
       // 挂到mark的transform上
       (this._wordMark as ICompilableMark).getProduct().transform(wordCloudTransforms);
@@ -361,54 +339,100 @@ export class BaseWordCloudSeries<T extends IBaseWordCloudSeriesSpec = IBaseWordC
       wordCloudTransforms.push({
         type: 'wordcloudShape',
         // 形状词云中必须要传入dataIndexKey, 否则填充词无法绘制
-        dataIndexKey: DEFAULT_DATA_KEY,
-
-        size: [width, height],
-        shape: this._maskShape,
-
-        text: { field: this._spec.word?.formatMethod ? WORD_CLOUD_TEXT : this._nameField },
-        fontSize: valueField ? { field: valueField } : this._fontSizeRange[0],
-        fontSizeRange: this._fontSizeRange,
-        padding: this._fontPadding,
-        rotateList: rotateAngles,
-        fontFamily: this._fontFamilyField ?? this._spec.word?.style?.fontFamily ?? this._defaultFontFamily,
-        fontWeight: fontWeightField ? { field: fontWeightField } : valueField ? { field: WORD_CLOUD_WEIGHT } : null,
-        fontStyle: this._fontStyleField ?? this._spec.word?.style?.fontStyle,
-
-        fillingFontFamily:
-          this._wordCloudShapeConfig?.fillingFontFamilyField ??
-          this._spec.word?.style?.fontFamily ??
-          this._defaultFontFamily,
-        fillingPadding: this._fillingFontPadding,
-        fillingFontStyle: this._wordCloudShapeConfig?.fillingFontStyleField ?? this._spec.word?.style?.fontStyle,
-        fillingFontWeight: this._wordCloudShapeConfig?.fillingFontWeightField ?? this._spec.word?.style?.fontWeight, // 填充词fontWeight默认不跟随valueField
-        fillingRotateList: fillingRotateAngles,
-
-        fillingTimes: this._wordCloudShapeConfig?.fillingTimes,
-        fillingXStep: this._wordCloudShapeConfig?.fillingXStep,
-        fillingYStep: this._wordCloudShapeConfig?.fillingYStep,
-        fillingXRatioStep: this._wordCloudShapeConfig?.fillingXRatioStep,
-        fillingYRatioStep: this._wordCloudShapeConfig?.fillingYRatioStep,
-        fillingInitialOpacity: this._wordCloudShapeConfig?.fillingInitialOpacity,
-        fillingDeltaOpacity: this._wordCloudShapeConfig?.fillingDeltaOpacity,
-        fillingInitialFontSize: this._wordCloudShapeConfig?.fillingInitialFontSize,
-        fillingDeltaFontSize: this._wordCloudShapeConfig?.fillingDeltaFontSize,
-
-        ratio: this._wordCloudShapeConfig?.ratio,
-        fillingRatio: this._wordCloudShapeConfig?.fillingRatio,
-        removeWhiteBorder: this._wordCloudShapeConfig?.removeWhiteBorder,
-        textLayoutTimes: this._wordCloudShapeConfig?.textLayoutTimes,
-        fontSizeShrinkFactor: this._wordCloudShapeConfig?.fontSizeShrinkFactor,
-        stepFactor: this._wordCloudShapeConfig?.stepFactor,
-        layoutMode: this._wordCloudShapeConfig?.layoutMode,
-        importantWordCount: this._wordCloudShapeConfig?.importantWordCount,
-        globalShinkLimit: this._wordCloudShapeConfig?.globalShinkLimit,
-        fontSizeEnlargeFactor: this._wordCloudShapeConfig?.fontSizeEnlargeFactor,
-        fillingDeltaFontSizeFactor: this._wordCloudShapeConfig?.fillingDeltaFontSizeFactor
+        ...this._wordCloudShapeTransformOption()
       });
     }
     // 把transform挂载到data的product上
     this._data.getProduct().transform(wordCloudTransforms);
+  }
+
+  protected _wordCloudTransformOption(): Object {
+    const { width, height } = this._region.getLayoutRect();
+    const wordStyleSpec = this._spec.word?.style ?? {};
+
+    return {
+      // TIP: 非浏览器环境下，使用 fast 布局，否则会出现兼容问题
+      layoutType: !isTrueBrowser(this._option.mode) ? 'fast' : this._wordCloudConfig.layoutMode,
+      size: [width, height],
+      shape: this._maskShape,
+      dataIndexKey: DEFAULT_DATA_KEY,
+      text: { field: this._textField },
+      fontSize: this._valueField ? { field: this._valueField } : this._fontSizeRange[0],
+      fontSizeRange: this._fontSizeRange,
+      padding: this._fontPadding,
+      rotate: { field: WORD_CLOUD_ANGLE },
+      fontFamily: this._fontFamilyField ?? wordStyleSpec.fontFamily ?? this._defaultFontFamily,
+      fontWeight: this._fontWeightField
+        ? { field: this._fontWeightField }
+        : this._valueField
+        ? { field: WORD_CLOUD_WEIGHT }
+        : null,
+      fontStyle: this._fontStyleField ?? wordStyleSpec.fontStyle,
+
+      randomVisible: this._random,
+      clip: this._wordCloudConfig.drawOutOfBound === 'clip',
+      shrink: this._wordCloudConfig.zoomToFit.shrink,
+      enlarge: this._wordCloudConfig.zoomToFit.enlarge,
+      minFontSize: this._wordCloudConfig.zoomToFit.fontSizeLimitMin,
+      progressiveTime: this._wordCloudConfig.progressiveTime,
+      progressiveStep: this._wordCloudConfig.progressiveStep
+    };
+  }
+
+  protected _wordCloudShapeTransformOption(): Object {
+    const { width, height } = this._region.getLayoutRect();
+    const wordStyleSpec = this._spec.word?.style ?? {};
+    const wordCloudShapeConfig = this._wordCloudShapeConfig ?? {};
+    const fillingRotateAngles = this._wordCloudShapeConfig.fillingRotateAngles;
+
+    return {
+      dataIndexKey: DEFAULT_DATA_KEY,
+
+      size: [width, height],
+      shape: this._maskShape,
+
+      text: { field: this._textField },
+      fontSize: this._valueField ? { field: this._valueField } : this._fontSizeRange[0],
+      fontSizeRange: this._fontSizeRange,
+      padding: this._fontPadding,
+      rotateList: this._rotateAngles,
+      fontFamily: this._fontFamilyField ?? wordStyleSpec.fontFamily ?? this._defaultFontFamily,
+      fontWeight: this._fontWeightField
+        ? { field: this._fontWeightField }
+        : this._valueField
+        ? { field: WORD_CLOUD_WEIGHT }
+        : null,
+      fontStyle: this._fontStyleField ?? wordStyleSpec.fontStyle,
+
+      fillingFontFamily:
+        wordCloudShapeConfig.fillingFontFamilyField ?? wordStyleSpec.fontFamily ?? this._defaultFontFamily,
+      fillingPadding: this._fillingFontPadding,
+      fillingFontStyle: wordCloudShapeConfig.fillingFontStyleField ?? wordStyleSpec.fontStyle,
+      fillingFontWeight: wordCloudShapeConfig.fillingFontWeightField ?? wordStyleSpec.fontWeight, // 填充词fontWeight默认不跟随valueField
+      fillingRotateList: fillingRotateAngles,
+
+      fillingTimes: wordCloudShapeConfig.fillingTimes,
+      fillingXStep: wordCloudShapeConfig.fillingXStep,
+      fillingYStep: wordCloudShapeConfig.fillingYStep,
+      fillingXRatioStep: wordCloudShapeConfig.fillingXRatioStep,
+      fillingYRatioStep: wordCloudShapeConfig.fillingYRatioStep,
+      fillingInitialOpacity: wordCloudShapeConfig.fillingInitialOpacity,
+      fillingDeltaOpacity: wordCloudShapeConfig.fillingDeltaOpacity,
+      fillingInitialFontSize: wordCloudShapeConfig.fillingInitialFontSize,
+      fillingDeltaFontSize: wordCloudShapeConfig.fillingDeltaFontSize,
+
+      ratio: wordCloudShapeConfig.ratio,
+      fillingRatio: wordCloudShapeConfig.fillingRatio,
+      removeWhiteBorder: wordCloudShapeConfig.removeWhiteBorder,
+      textLayoutTimes: wordCloudShapeConfig.textLayoutTimes,
+      fontSizeShrinkFactor: wordCloudShapeConfig.fontSizeShrinkFactor,
+      stepFactor: wordCloudShapeConfig.stepFactor,
+      layoutMode: wordCloudShapeConfig.layoutMode,
+      importantWordCount: wordCloudShapeConfig.importantWordCount,
+      globalShinkLimit: wordCloudShapeConfig.globalShinkLimit,
+      fontSizeEnlargeFactor: wordCloudShapeConfig.fontSizeEnlargeFactor,
+      fillingDeltaFontSizeFactor: wordCloudShapeConfig.fillingDeltaFontSizeFactor
+    };
   }
 
   getStatisticFields() {
