a7a7f6652d561abae5a2ddc4a54b78e87435f84b
xile611 <dingling112@gmail.com>
2023-12-21 18:27:12 +0800

    fix: fix error in strict mode of react-vchart, fix #1669

--- packages/react-vchart/src/VChartSimple.tsx
+++ packages/react-vchart/src/VChartSimple.tsx
@@ -1,5 +1,6 @@
+import type { IVChartConstructor } from '@visactor/vchart';
 import { BaseChartProps, createChart } from './charts/BaseChart';
 
-export type VChartSimpleProps = Omit<BaseChartProps, 'container' | 'data' | 'width' | 'height'>;
+export type VChartSimpleProps = Omit<BaseChartProps, 'container' | 'data' | 'width' | 'height' | 'vchartConstrouctor'>;
 
-export const VChartSimple = createChart<VChartSimpleProps>('VChartSimple');
+export const VChartSimple = createChart<VChartSimpleProps & { vchartConstrouctor: IVChartConstructor }>('VChartSimple');
