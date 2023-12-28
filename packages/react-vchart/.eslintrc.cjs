3823e10f8d5ab76cc7de2992e9d4dc312370ea49
xile611 <dingling112@gmail.com>
2023-12-28 11:39:05 +0800

    feat: export version in react-vchart

--- packages/react-vchart/.eslintrc.cjs
+++ packages/react-vchart/.eslintrc.cjs
@@ -2,6 +2,11 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   extends: ['@internal/eslint-config/profile/react'],
+  globals: {
+    __DEV__: 'readonly',
+    __VERSION__: 'readonly',
+    NodeJS: true
+  },
   parserOptions: { tsconfigRootDir: __dirname, project: './tsconfig.eslint.json' },
   // ignorePatterns: [],
   rules: {
