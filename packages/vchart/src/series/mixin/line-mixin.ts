ef01dd1d861857ef8349024e06a43f1bd9fe42cd
skie1997 <skie1997@outlook.com>
2023-12-28 19:12:21 +0800

    fix: polar axis overlap error

--- packages/vchart/src/series/mixin/line-mixin.ts
+++ packages/vchart/src/series/mixin/line-mixin.ts
@@ -108,7 +108,7 @@ export class LineLikeSeriesMixin {
       const overlapTrans = [];
       overlapTrans.push({
         type: 'markoverlap',
-        direction: this._direction === Direction.vertical ? 1 : 2,
+        direction: this._direction === Direction.horizontal && this.coordinate === 'cartesian' ? 2 : 1,
         delta: this._spec.pointDis,
         deltaMul: this._spec.pointDisMul,
         groupBy: this._seriesField
