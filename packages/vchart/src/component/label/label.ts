c9e8f6401af9ca9402161bd0c5f550ab9bcb5e8f
xiaoluoHe <sevnape@outlook.com>
2024-01-12 17:11:42 +0800

    fix: label position incorrect with indent config, closes #2013

--- packages/vchart/src/component/label/label.ts
+++ packages/vchart/src/component/label/label.ts
@@ -305,7 +305,9 @@ export class Label<T extends IChartSpec = any> extends BaseLabelComponent<T> {
                   .getAllComponents()
                   .filter(cmp => cmp.type === 'totalLabel')
                   .map(cmp => cmp.getMarks()[0].getProductId())
-              }
+              },
+              x: labelInfos[0].series.getRegion().layout.indent.left,
+              y: labelInfos[0].series.getRegion().layout.indent.top
             },
             configFunc(labelInfo),
             {
@@ -330,7 +332,7 @@ export class Label<T extends IChartSpec = any> extends BaseLabelComponent<T> {
             : textAttribute(labelInfos[params.labelIndex], datum, labelSpec.formatMethod, labelSpec.formatter);
         }
       })
-      .size(() => labelInfos[0].series.getRegion().getLayoutRect());
+      .size(() => labelInfos[0].series.getRegion().getLayoutRectExcludeIndent());
   }
 
   compileMarks() {
