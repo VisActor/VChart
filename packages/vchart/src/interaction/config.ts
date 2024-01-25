a1b89f689a1ca08a522e2f13c334b6ed9f927a39
xile611 <dingling112@gmail.com>
2024-01-25 19:15:22 +0800

    perf: optimize the performance of element-highlight

--- packages/vchart/src/interaction/config.ts
+++ packages/vchart/src/interaction/config.ts
@@ -7,8 +7,8 @@ export function getDefaultInteractionConfigByMode(mode: RenderMode) {
     return {
       hover: {
         enable: true,
-        trigger: 'pointerover',
-        triggerOff: 'pointerout'
+        trigger: 'pointermove',
+        triggerOff: 'view:pointerleave'
       },
       select: {
         enable: true,
@@ -21,8 +21,8 @@ export function getDefaultInteractionConfigByMode(mode: RenderMode) {
     return {
       hover: {
         enable: true,
-        trigger: ['pointerdown', 'pointerover'],
-        triggerOff: 'pointerout'
+        trigger: ['pointerdown', 'pointermove'],
+        triggerOff: 'view:pointerleave'
       },
       select: {
         enable: true,
