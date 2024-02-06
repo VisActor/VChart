485e8327d7d55171ec8d55c991d4b907d6e10142
kkxxkk2019 <just_star@qq.com>
2024-02-06 14:41:14 +0800

    chore: update rush change

--- packages/vchart/__tests__/runtime/browser/test-page/debug.ts
+++ packages/vchart/__tests__/runtime/browser/test-page/debug.ts
@@ -3,193 +3,249 @@ import { default as VChart } from '../../../../src/index';
 const CONTAINER_ID = 'chart';
 
 const run = async () => {
-  // const spec = {
-  //   type: 'bar',
-  //   yField: 'value',
-  //   stack: false,
-  //   label: {
-  //     visible: true,
-  //     position: 'outside'
-  //   },
-  //   legends: [
-  //     {
-  //       visible: true
-  //     }
-  //   ],
-  //   xField: ['filterKey', 'type'],
-  //   seriesField: 'filterKey',
-  //   axes: [
-  //     {
-  //       orient: 'bottom',
-  //       visible: true,
-  //       showAllGroupLayers: true,
-  //       layers: [
-  //         {
-  //           visible: true
-  //         },
-  //         {
-  //           visible: false
-  //         }
-  //       ],
-  //       label: {
-  //         visible: true
-  //       }
-  //     },
-  //     {
-  //       orient: 'left',
-  //       visible: true,
-  //       zero: false,
-  //       label: {
-  //         visible: true
-  //       }
-  //     }
-  //   ],
-  //   data: [
-  //     {
-  //       name: 'bar',
-  //       values: [
-  //         {
-  //           type: 'normal',
-  //           filterKey: '非首日（1）',
-  //           value: 3531
-  //         },
-  //         {
-  //           type: 'compare',
-  //           filterKey: '非首日（1）',
-  //           value: 3530
-  //         },
-  //         {
-  //           type: 'normal',
-  //           filterKey: '已登录（2）',
-  //           value: 0
-  //         },
-  //         {
-  //           type: 'compare',
-  //           filterKey: '已登录（2）',
-  //           value: 0
-  //         }
-  //       ]
-  //     }
-  //   ]
-  // };
-
   const spec = {
-    type: 'bar',
-    data: [
-      {
-        values: [
-          {
-            type: 'Category One',
-            min: 76,
-            max: 100,
-            range: 'A',
-            type2: 'p',
-            color: 'A_p'
-          },
-          {
-            type: 'Category Two',
-            min: 56,
-            max: 108,
-            range: 'A',
-            type2: 'p',
-            color: 'A_p'
-          },
-          {
-            type: 'Category One',
-            min: 56,
-            max: 100,
-            range: 'B',
-            type2: 'p',
-            color: 'B_p'
-          },
-          {
-            type: 'Category Two',
-            min: 36,
-            max: 108,
-            range: 'B',
-            type2: 'p',
-            color: 'B_p'
-          },
-
-          {
-            type: 'Category One',
-            min: 76,
-            max: 100,
-            range: 'A',
-            type2: 'k',
-            color: 'A_k'
-          },
-          {
-            type: 'Category Two',
-            min: 56,
-            max: 108,
-            range: 'A',
-            type2: 'k',
-            color: 'A_k'
-          },
-          {
-            type: 'Category One',
-            min: 56,
-            max: 100,
-            range: 'B',
-            type2: 'k',
-            color: 'B_k'
-          },
-          {
-            type: 'Category Two',
-            min: 36,
-            max: 108,
-            range: 'B',
-            type2: 'k',
-            color: 'B_k'
-          }
-        ]
-      }
-    ],
-    xField: ['type', 'range', 'type2'],
-    yField: 'min',
-    seriesField: 'color',
-    // barGapInGroup: [0, 5],
-    barWidth: 20,
-    paddingInner: [0.6, 0.6, 0.6],
-    bandPadding: [0.6, 0.6, 0.6],
-    label: {
-      position: 'bothEnd'
-    },
+    type: 'scatter',
     axes: [
       {
-        orient: 'bottom',
-        showAllGroupLayers: true,
-        layers: [
-          {
-            visible: true
-          },
-          {
-            visible: false
-            // tickCount: 2
-          },
-          {
-            // visible: false
-          }
-        ],
-        // sampling: false,
-        label: {
-          // autoRotate: true
+        orient: 'left',
+        type: 'linear',
+        grid: {
+          visible: false
         },
-        tick: {
-          tickCount: 2
+        domainLine: {
+          visible: true
+        },
+        max: 0.1,
+        min: 0,
+        label: {},
+        id: 'yAxis'
+      },
+      {
+        orient: 'bottom',
+        type: 'linear',
+        grid: {
+          visible: false
+        },
+        domainLine: {
+          visible: true
+        },
+        max: 0.01,
+        min: 0,
+        label: {}
+      }
+    ],
+    markArea: [
+      {
+        x: '50%',
+        y: '0%',
+        x1: '100%',
+        y1: '50%',
+        area: {
+          style: {
+            fill: 'transparent',
+            stroke: '#E6E8F2'
+          }
+        }
+      },
+      {
+        x: '0%',
+        y: '0%',
+        x1: '50%',
+        y1: '50%',
+        area: {
+          style: {
+            fill: 'rgba(255, 187, 51, 0.7)',
+            stroke: '#E6E8F2'
+          }
+        }
+      },
+      {
+        x: '0%',
+        y: '50%',
+        x1: '50%',
+        y1: '100%',
+        area: {
+          style: {
+            fill: 'transparent',
+            stroke: '#E6E8F2'
+          }
+        }
+      },
+      {
+        x: '50%',
+        y: '50%',
+        x1: '100%',
+        y1: '100%',
+        area: {
+          style: {
+            fill: 'rgba(64, 113, 255, 0.7)',
+            stroke: '#E6E8F2'
+          }
         }
       }
     ],
-    legends: {
-      visible: true
-    }
+    markLine: [
+      {
+        x: '0%',
+        y: '100%',
+        x1: '100%',
+        y1: '0%',
+        line: {
+          style: {
+            lineDash: [0],
+            stroke: '#E6E8F2'
+          }
+        },
+        endSymbol: {
+          visible: false
+        },
+        zIndex: 0
+      }
+    ],
+    extensionMark: [
+      {
+        type: 'text',
+        style: {
+          fontSize: 20,
+          fill: '#1F2129',
+          textBaseline: 'top',
+          zIndex: 1,
+          y: 12,
+          textAlign: 'end',
+          text: '第一象限'
+        }
+      },
+      {
+        type: 'text',
+        style: {
+          fontSize: 20,
+          fill: '#1F2129',
+          textBaseline: 'top',
+          zIndex: 1,
+          x: 8,
+          y: 12,
+          textAlign: 'start',
+          text: '第二象限'
+        }
+      },
+      {
+        type: 'text',
+        style: {
+          fontSize: 20,
+          fill: '#1F2129',
+          textBaseline: 'bottom',
+          zIndex: 1,
+          x: 8,
+          textAlign: 'start',
+          text: '第三象限'
+        }
+      },
+      {
+        type: 'text',
+        style: {
+          fontSize: 20,
+          fill: '#1F2129',
+          textBaseline: 'bottom',
+          zIndex: 1,
+          textAlign: 'end',
+          text: '第四象限'
+        }
+      }
+    ],
+    padding: [0, 30, 30, 30],
+    data: {
+      values: []
+    },
+    color: ['#FF995A'],
+    xField: 'x_rate',
+    yField: 'y_rate',
+    size: 48,
+    seriesField: 'type',
+    point: {
+      state: {
+        hover: {
+          scaleX: 1.2,
+          scaleY: 1.2,
+          fillOpacity: 1,
+          cursor: 'pointer',
+          zIndex: 99
+        }
+      },
+      style: {
+        fillOpacity: 0.8,
+        zIndex: 3
+      }
+    },
+    legends: [
+      {
+        visible: true,
+        orient: 'top',
+        position: 'middle'
+      }
+    ],
+    dataZoom: [
+      {
+        filterMode: 'axis',
+        orient: 'bottom',
+        start: 0,
+        end: 1,
+        id: 'xDataZoom',
+        offsetY: 20,
+        showDetail: false
+      },
+      {
+        filterMode: 'axis',
+        orient: 'right',
+        axisId: 'yAxis',
+        offsetX: 20,
+        start: 0,
+        end: 1,
+        id: 'yDataZoom',
+        showDetail: false
+      }
+    ],
+    brush: {
+      visible: false,
+      brushType: 'rect',
+      inBrush: {
+        colorAlpha: 1
+      },
+      outOfBrush: {
+        colorAlpha: 0.2
+      }
+    },
+    tooltip: {
+      activeType: 'mark',
+      mark: {
+        title: {},
+        content: [
+          {
+            key: '线索用户覆盖度'
+          },
+          {
+            key: '深度兴趣用户覆盖度'
+          },
+          {
+            key: '有效阅读线索用户数',
+            visible: false
+          }
+        ]
+      }
+    },
+    label: {
+      position: 'center',
+      visible: true,
+      style: {
+        fontSize: 10,
+        fill: '#fff',
+        strokeOpacity: 0,
+        zIndex: 1
+      },
+      overlap: false
+    },
+    animation: false
   };
-
   const vchart = new VChart(spec, { dom: CONTAINER_ID });
   vchart.renderSync();
-
-  // Just for the convenience of console debugging, DO NOT COPY!
-  window['vchart'] = vchart;
 };
 run();
