99030bee89c13cee3dc81945417087dabccd7531
kkxxkk2019 <just_star@qq.com>
2024-03-11 12:28:33 +0800

    fix: grid component should not be pickable expect its children

--- packages/vchart/src/component/axis/cartesian/axis.ts
+++ packages/vchart/src/component/axis/cartesian/axis.ts
@@ -388,6 +388,7 @@ export abstract class CartesianAxis<T extends ICartesianAxisCommonSpec = ICartes
         }
       });
     }
+
     this.callPlugin(plugin => {
       this.pluginService && plugin.onDidCompile && plugin.onDidCompile(this.pluginService, this);
     });
