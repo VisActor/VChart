dfcc39bdf926ba410660874cca87c363444cff38
skie1997 <skie1997@outlook.com>
2024-03-18 19:08:55 +0800

    chore: add notes

--- packages/vchart/src/component/data-zoom/data-zoom/interface.ts
+++ packages/vchart/src/component/data-zoom/data-zoom/interface.ts
@@ -56,7 +56,8 @@ export interface IDataZoomSpec extends IDataZoomStyle, IDataFilterComponentSpec 
   /**
    * 数据过滤模式
    * @default 'filter' （dataZoom默认数据过滤模式）
-   * 详细可参考：https://echarts.apache.org/zh/option.html#dataZoom-slider.filterMode）
+   * 'filter' 为过滤数据从而达到缩放轴的效果, 'axis'为直接缩放轴, 不过滤数据
+   * 具体效果可参考: https://www.visactor.io/vchart/demo/sequence-chart/social-media-event?keyword=dataZoom
    */
   filterMode?: IFilterMode;
   /**
--- packages/vchart/src/component/data-zoom/scroll-bar/interface.ts
+++ packages/vchart/src/component/data-zoom/scroll-bar/interface.ts
@@ -20,7 +20,8 @@ export interface IScrollBarSpec extends IDataFilterComponentSpec, IScrollBarStyl
   /**
    * 数据过滤模式
    * @default 'axis' (scrollBar默认视口裁剪模式)
-   * 详细可参考：https://echarts.apache.org/zh/option.html#dataZoom-slider.filterMode）
+   * 'filter' 为过滤数据从而达到缩放轴的效果, 'axis'为直接缩放轴, 不过滤数据
+   * 具体效果可参考: https://www.visactor.io/vchart/demo/sequence-chart/social-media-event?keyword=dataZoom
    */
   filterMode?: IFilterMode;
   /** 滑块是否圆角。 */
