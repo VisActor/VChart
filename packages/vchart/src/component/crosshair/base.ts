cbba5f81b09bc51e186ab7f54d941331a0c06d2e
Howard Zhang <zamhown@live.com>
2024-03-20 17:45:57 +0800

    fix: `lineWidth` is invalid in rect-type-crosshair

--- packages/vchart/src/component/crosshair/base.ts
+++ packages/vchart/src/component/crosshair/base.ts
@@ -375,17 +375,12 @@ export abstract class BaseCrossHair<T extends ICartesianCrosshairSpec | IPolarCr
       hair.style = { visible: false };
     } else {
       const style = line.style || {};
-      const { strokeOpacity, fillOpacity, opacity, stroke, fill, lineWidth, ...restStyle } = style as any;
+      const { stroke, fill, lineWidth } = style as any;
       const isLineType = hair.type === 'line';
-      let finalOpacity = isLineType ? strokeOpacity : fillOpacity;
-      if (isNumber(opacity)) {
-        finalOpacity = (finalOpacity ?? 1) * opacity;
-      }
       hair.style = {
-        opacity: finalOpacity,
         pickable: false,
         visible: true,
-        ...restStyle
+        ...style
       };
 
       if (isLineType) {
