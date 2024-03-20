55f47ff7083c245ee739e3f310aa52ed371b0e92
xile611 <github-actions[bot]@users.noreply.github.com>
2024-03-20 08:12:48 +0000

    build: prelease version 1.8.5-hotfix.4

--- packages/vchart-types/types/component/legend/util.d.ts
+++ packages/vchart-types/types/component/legend/util.d.ts
@@ -15,9 +15,15 @@ export declare function transformLegendTitleAttributes(title: ITitle): {
         visible?: boolean;
         style?: Omit<import("./interface").NoVisibleMarkStyle<import("../..").IRectMarkSpec>, "width" | "height" | "visible">;
     };
-    text?: string | number | string[] | number[];
+    text?: string | number | string[] | number[] | {
+        type?: "text";
+        text: string | number | string[] | number[];
+    } | {
+        type: "rich";
+        text: import("@visactor/vrender-core").IRichTextCharacter[];
+    };
     visible?: boolean;
-    padding?: import("@visactor/vrender-components/es/core/type").Padding;
+    padding?: import("@visactor/vrender-components").Padding;
     maxWidth?: number;
     minWidth?: number;
     space?: number;
