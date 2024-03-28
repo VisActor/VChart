c4e31797e2a739733071b347abe49a5547ba0732
Howard Zhang <zamhown@live.com>
2024-03-28 11:07:20 +0800

    fix: remove exports

--- packages/vchart/src/plugin/components/tooltip-handler/utils/index.ts
+++ packages/vchart/src/plugin/components/tooltip-handler/utils/index.ts
@@ -1,6 +1,4 @@
 export * from './common';
-export * from '../../../../component/tooltip/utils/compose';
 export * from './pattern';
-export * from '../../../../component/tooltip/utils/get-spec';
 export * from './position';
 export * from './attribute';
