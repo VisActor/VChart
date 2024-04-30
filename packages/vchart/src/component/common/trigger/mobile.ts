eafcb3f2f1e64c25086ccef774ab844972bfd548
skie1997 <skie1997@outlook.com>
2024-04-30 16:39:12 +0800

    fix(scroll): scroll not work in ios. fix#1224

--- packages/vchart/src/component/common/trigger/mobile.ts
+++ packages/vchart/src/component/common/trigger/mobile.ts
@@ -9,10 +9,13 @@ export class MobileTrigger implements IZoomTrigger {
   }
 
   parserDragEvent(event: any): boolean {
-    if (this.pointerId) {
-      return this.pointerId === event.pointerId;
-    }
-    this.pointerId = event.pointerId;
+    // 下面的代码逻辑本意是：单指(pointerId===0) -> 多指(pointerId === 1) 切换后, 终止touch事件
+    // 问题：ios系统/部分MIUI系统，单指/多指滑动过程中, pointerId每次都不一样，所以无法做到上述识别
+    // 解决方法: 暂时不做流程判断, 如果后续有需求, 单指 -> 多指可以考虑用event.isPrimary判断
+    // if (this.pointerId) {
+    //   return this.pointerId === event.pointerId;
+    // }
+    // this.pointerId = event.pointerId;
     return true;
   }
 
