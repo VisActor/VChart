45b4044dd9654a656f1511e3b300f60ad0ae2b53
skie1997 <skie1997@outlook.com>
2024-05-26 19:50:48 +0800

    chore: type error

--- packages/vchart/src/chart/common/common-transformer.ts
+++ packages/vchart/src/chart/common/common-transformer.ts
@@ -30,7 +30,7 @@ export class CommonChartSpecTransformer<T extends ICommonChartSpec = ICommonChar
       if (series.type === 'bar' && !!series.autoBandSize) {
         const isHorizontal = series.direction === 'horizontal';
         const matchOrient = isHorizontal ? ['left', 'right'] : ['top', 'bottom'];
-        const relatedAxis: any = spec.axes.find((axis: any) => {
+        const relatedAxis: any = (spec.axes as any).find((axis: any) => {
           if (!matchOrient.includes(axis.orient)) {
             // orient必须匹配
             return false;
