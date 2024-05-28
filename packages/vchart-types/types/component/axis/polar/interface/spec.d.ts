590e645283328fd3b54075c6a7b40d27ec900ffe
skie1997 <github-actions[bot]@users.noreply.github.com>
2024-05-28 03:49:22 +0000

    build: prelease version 1.11.2-alpha.4

--- packages/vchart-types/types/component/axis/polar/interface/spec.d.ts
+++ packages/vchart-types/types/component/axis/polar/interface/spec.d.ts
@@ -1,8 +1,9 @@
-import type { IPoint, IPolarOrientType } from '../../../../typings';
+import type { ILayoutRect, IPoint, IPolarOrientType } from '../../../../typings';
 import type { IBandAxisSpec, IDomainLine, ILinearAxisSpec, ITitle, ILabel, ICommonAxisSpec } from '../../interface';
 import type { IPolarGrid } from './common';
 export type IPolarAxisSpec = IPolarLinearAxisSpec | IPolarBandAxisSpec;
 export type IPolarAxisCommonSpec = Omit<ICommonAxisSpec, 'center'> & {
+    layoutRadius?: 'auto' | number | ((layoutRect: ILayoutRect, center: IPoint) => number);
     inside?: boolean;
     orient: IPolarOrientType;
     grid?: IPolarGrid;
@@ -13,7 +14,10 @@ export type IPolarAxisCommonSpec = Omit<ICommonAxisSpec, 'center'> & {
     title?: ITitle;
     innerRadius?: number;
     outerRadius?: number;
-    center?: IPoint;
+    center?: {
+        x: number | string;
+        y: number | string;
+    };
     startAngle?: number;
     endAngle?: number;
 };
--- packages/vchart-types/types/util/space.d.ts
+++ packages/vchart-types/types/util/space.d.ts
@@ -5,7 +5,7 @@ import type { ILayoutNumber, ILayoutRect, IPercent, IPercentOffset, ILayoutPaddi
 export declare function isValidOrient(orient: string): boolean;
 export declare function isPercent(v: any): v is IPercent;
 export declare function isPercentOffset(v: any): v is IPercentOffset;
-export declare function calcLayoutNumber(v: ILayoutNumber | undefined, size: number, callOp?: ILayoutRect): number;
+export declare function calcLayoutNumber(v: ILayoutNumber | undefined, size: number, callOp?: ILayoutRect, defaultValue?: number): number;
 export declare function calcPadding(paddingSpec: ILayoutOrientPadding, rect: ILayoutRect, callOp: ILayoutRect): IPadding;
 export declare function boundsInRect(bounds: IBoundsLike, rect: ILayoutRect): ILayoutRect;
 export declare function normalizeLayoutPaddingSpec(spec: ILayoutPaddingSpec): ILayoutOrientPadding;
