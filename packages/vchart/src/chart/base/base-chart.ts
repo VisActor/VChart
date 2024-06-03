1146d7da4db628870bb93be8db926fd899e29a81
lixuefei.1313 <lixuef1313@163.com>
2024-06-03 14:11:28 +0800

    fix: fixed the issue that the newly added component type could not take effect when updateSpec

--- packages/vchart/src/chart/base/base-chart.ts
+++ packages/vchart/src/chart/base/base-chart.ts
@@ -790,7 +790,7 @@ export class BaseChart<T extends IChartSpec> extends CompilableBase implements I
       result.reMake = true;
       return result;
     }
-    // spec key 的个数一致，但是内不一致时。remake
+    // spec key 的个数一致，但是数组长度不一致时。remake
     for (let i = 0; i < currentKeys.length; i++) {
       const key = currentKeys[i];
       if (isArray(this._spec[key]) && this._spec[key].length !== spec[key].length) {
