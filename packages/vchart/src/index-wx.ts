937df22939e93d425d6140df29ed03fa40f08f71
xile611 <dingling112@gmail.com>
2024-08-07 19:07:24 +0800

    fix: fix path error in index-wx

--- packages/vchart/src/index-wx.ts
+++ packages/vchart/src/index-wx.ts
@@ -146,5 +146,6 @@ export { VChart };
 export default VChart;
 
 export * from './core';
-
-export * from './plugin';
+export * from './plugin/chart';
+export * from './plugin/components/tooltip-handler';
+export * from './plugin/components/axis-sync';
