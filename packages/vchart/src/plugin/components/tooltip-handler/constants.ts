389b110618c7c2e73e6f179e826e8ce281906cef
xile611 <dingling112@gmail.com>
2024-10-24 19:10:43 +0800

    feat: support max height of tooltip, by percent value , close #2675

--- packages/vchart/src/plugin/components/tooltip-handler/constants.ts
+++ packages/vchart/src/plugin/components/tooltip-handler/constants.ts
@@ -2,6 +2,7 @@ import type { ITooltipHandlerOptions } from './interface';
 
 export const TOOLTIP_PREFIX = 'vchart-tooltip';
 export const TOOLTIP_CONTAINER_EL_CLASS_NAME = `${TOOLTIP_PREFIX}-container`;
+export const TOOLTIP_TITLE_CLASS_NAME = `${TOOLTIP_PREFIX}-title`;
 export const TOOLTIP_CONTENT_BOX_CLASS_NAME = `${TOOLTIP_PREFIX}-content-box`;
 export const TOOLTIP_EMPTY_STRING = '';
 
