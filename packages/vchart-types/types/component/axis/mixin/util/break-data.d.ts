ecea79c9f13cfc046e6e694a53e933a47edc0e91
xile611 <github-actions[bot]@users.noreply.github.com>
2024-11-14 11:09:03 +0000

    build: prelease version 1.12.12-alpha.0

--- packages/vchart-types/types/component/axis/mixin/util/break-data.d.ts
+++ packages/vchart-types/types/component/axis/mixin/util/break-data.d.ts
@@ -1,4 +1,4 @@
-export declare function breakData(data: number[], points: number[]): {
+export declare function breakData(data: number[], points: number[], scopeType?: 'count' | 'length'): {
     domain: [number, number][];
     scope: [number, number][];
 };
