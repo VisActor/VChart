81c82f9e9e65c830b91e647de34c3e69b157a615
xile611 <dingling112@gmail.com>
2024-12-04 10:05:42 +0800

    chore: update bugserver-trigger

--- tools/bugserver-trigger/src/index.ts
+++ tools/bugserver-trigger/src/index.ts
@@ -1,5 +1,5 @@
 import VChart from '@visactor/vchart';
 import * as VChartExtensions from '@visactor/vchart-extension';
 
+(VChart as any).VChartExtensions = VChartExtensions;
 export default VChart;
-export { VChartExtensions };
