3af43de6ab8e93c4d8fcc9d11e6f96e0215ffe17
xile611 <dingling112@gmail.com>
2024-12-09 19:07:43 +0800

    chore: tooltip should not has animation in bug server

--- tools/bugserver-trigger/src/index.ts
+++ tools/bugserver-trigger/src/index.ts
@@ -2,6 +2,11 @@ import * as VChart from '@visactor/vchart';
 import * as VChartExtension '@visactor/vchart-extension';
 
 
+const defaultTheme = VChart.ThemeManager.getDefaultTheme();
+
+//  强制设置tooltip不进行动画 
+defaultTheme.component.tooltip.transitionDuration = 0;
+
 (window as any).VChart = VChart;
 (window as any).VChartExtension = VChartExtension;
 
