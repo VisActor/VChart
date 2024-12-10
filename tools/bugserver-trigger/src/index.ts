3de845ac68e3de88c0c80da6a4dabd8ffdc291d5
xile611 <dingling112@gmail.com>
2024-12-10 17:31:54 +0800

    chore: disable transform of tooltip in bugserver

--- tools/bugserver-trigger/src/index.ts
+++ tools/bugserver-trigger/src/index.ts
@@ -2,10 +2,12 @@ import * as VChart from '@visactor/vchart';
 import * as VChartExtension '@visactor/vchart-extension';
 
 
-const defaultTheme = VChart.ThemeManager.getDefaultTheme();
+const defaultTheme = VChart.ThemeManager.getCurrentTheme();
+const defaultTransformedTheme = VChart.ThemeManager.getCurrentTheme(true);
 
 //  强制设置tooltip不进行动画 
 defaultTheme.component.tooltip.transitionDuration = 0;
+defaultTransformedTheme.component.tooltip.transitionDuration = 0;
 
 (window as any).VChart = VChart;
 (window as any).VChartExtension = VChartExtension;
