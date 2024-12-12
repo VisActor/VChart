32ad8a5dc4146cf5f2a3d75229c92dea81da5f03
zhouxinyu <zhouxinyu12138@gmail.com>
2024-12-12 10:56:39 +0800

    feat: remove registerWXEnv

--- packages/vchart/src/index-harmony.ts
+++ packages/vchart/src/index-harmony.ts
@@ -2,7 +2,7 @@
  * @description 包含基础的折柱饼图，提供坐标轴、离散图例以及 tooltip、crosshair、label 组件
  */
 import { VChart } from './core';
-import { registerHarmonyEnv, registerWXEnv } from './env';
+import { registerHarmonyEnv } from './env';
 
 import { registerLineChart } from './chart/line';
 import { registerAreaChart } from './chart/area';
