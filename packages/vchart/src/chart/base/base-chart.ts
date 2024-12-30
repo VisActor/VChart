64309e1a2ff7b95e0ecd039ae86865adb4752736
xiaoluoHe <sevnape@outlook.com>
2024-12-30 14:45:10 +0800

    feat: remove map-label related code

--- packages/vchart/src/chart/base/base-chart.ts
+++ packages/vchart/src/chart/base/base-chart.ts
@@ -874,8 +874,7 @@ export class BaseChart<T extends IChartSpec> extends CompilableBase implements I
     } = {};
     const checkVisibleComponents: Record<string, boolean> = {
       [ComponentTypeEnum.title]: true,
-      [ComponentTypeEnum.brush]: true,
-      [ComponentTypeEnum.mapLabel]: true
+      [ComponentTypeEnum.brush]: true
     };
 
     this._components.forEach(c => {
