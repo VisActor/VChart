48e4ca4bfc83952da555eec98b110c80b524cad6
xiaoluoHe <sevnape@outlook.com>
2024-12-30 14:41:01 +0800

    docs: add guide for mapLabel

--- packages/vchart-extension/src/components/map-label/type.ts
+++ packages/vchart-extension/src/components/map-label/type.ts
@@ -23,15 +23,25 @@ export type IMapLabelNodes = 'nameLabel' | 'valueLabel' | 'icon' | 'labelBackgro
 export type MapLabelSceneNodeMap = Partial<Record<IMapLabelNodes, IGraphic>>;
 
 export interface IMapLabelStyleSpec {
-  /** 是否显示，默认 false */
+  /**
+   * 是否显示
+   * @default false
+   */
   visible?: boolean;
-  /** 标签非 outer 位置下与标记点的间距 */
+  /**
+   * 标签非 outer 位置下与标记点的间距
+   * @default 12
+   */
   offset?: number;
   /**
-   * icon/label之间的距离
+   * icon 和 label之间的距离
+   * @default 10
    */
   space?: number;
-  /** 标签位置 */
+  /**
+   * 标签位置。支持 'left' | 'top' | 'right' | 'bottom' | 'outer'
+   * @default 'top'
+   */
   position?: LabelPosition;
   /** 名称文本样式设置 */
   nameLabel?: {
@@ -50,7 +60,9 @@ export interface IMapLabelStyleSpec {
   };
   /** 背景样式设置 */
   background?: {
+    /** @default true */
     visible?: boolean;
+    /** 背景框边距 */
     padding?: IPadding;
     style?: IRectMarkSpec;
   };
