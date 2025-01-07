6bb775d62e22568d070e0d2a6244354e20355c0c
skie1997 <github-actions[bot]@users.noreply.github.com>
2025-01-07 18:29:17 +0000

    build: prelease version 1.13.3-alpha.8

--- packages/vchart-types/types/mark/utils/common.d.ts
+++ packages/vchart-types/types/mark/utils/common.d.ts
@@ -2,3 +2,4 @@ import { MarkTypeEnum } from '../interface/type';
 export declare const MultiDatumMark: string[];
 export declare function isMultiDatumMark(type: MarkTypeEnum): boolean;
 export declare function curveTypeTransform(type: string, direction: string): string;
+export declare function is3DMark(type: MarkTypeEnum): boolean;
