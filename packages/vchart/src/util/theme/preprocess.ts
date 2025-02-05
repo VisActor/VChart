9dc46a1ef7ca49c412bba83840dfc5d1f4678492
xile611 <dingling112@gmail.com>
2025-02-05 19:53:26 +0800

    refactor: optimize the process of theme

--- packages/vchart/src/util/theme/preprocess.ts
+++ packages/vchart/src/util/theme/preprocess.ts
@@ -1,7 +1,6 @@
 import { isPlainObject } from '@visactor/vutils';
 import type { IThemeColorScheme } from '../../theme/color-scheme/interface';
 import { getActualColor, isColorKey } from '../../theme/color-scheme/util';
-import type { ISeriesSpec } from '../../typings';
 import type { TokenMap } from '../../theme/token';
 // eslint-disable-next-line no-duplicate-imports
 import { isTokenKey, queryToken } from '../../theme/token';
@@ -9,12 +8,7 @@ import type { ITheme } from '../../theme';
 
 const IGNORE_KEYS = ['animationThreshold', 'colorScheme', 'name', 'padding'];
 
-export function preprocessTheme(
-  obj: any,
-  colorScheme?: IThemeColorScheme,
-  tokenMap?: TokenMap,
-  seriesSpec?: ISeriesSpec
-): any {
+export function preprocessTheme(obj: any, colorScheme?: IThemeColorScheme, tokenMap?: TokenMap): any {
   if (!obj) {
     return obj;
   }
@@ -28,19 +22,19 @@ export function preprocessTheme(
   Object.keys(obj).forEach(key => {
     const value = obj[key];
     if (IGNORE_KEYS.includes(key)) {
-      newObj[key] = value;
+      (newObj as any)[key] = value;
     } else if (isPlainObject(value)) {
       if (isColorKey(value)) {
         // 查询、替换语义化颜色
-        newObj[key] = getActualColor(value, colorScheme, seriesSpec);
+        (newObj as any)[key] = getActualColor(value, colorScheme);
       } else if (isTokenKey(value)) {
         // 查询、替换语义化 token
-        newObj[key] = queryToken(tokenMap, value);
+        (newObj as any)[key] = queryToken(tokenMap, value);
       } else {
-        newObj[key] = preprocessTheme(value, colorScheme, tokenMap, seriesSpec);
+        (newObj as any)[key] = preprocessTheme(value, colorScheme, tokenMap);
       }
     } else {
-      newObj[key] = value;
+      (newObj as any)[key] = value;
     }
   });
 
