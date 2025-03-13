797b0f362c0f2f816817fd7acc43b086bfd6d726
xile611 <dingling112@gmail.com>
2025-03-13 11:11:25 +0800

    fix: lifecycle event callback should be fired, fix #3792

--- packages/react-vchart/src/charts/BaseChart.tsx
+++ packages/react-vchart/src/charts/BaseChart.tsx
@@ -188,14 +188,16 @@ const BaseChart: React.FC<Props> = React.forwardRef((props, ref) => {
     isUnmount.current = false;
   };
 
-  const handleChartRender = () => {
+  const handleChartRender = (rebindEvent?: boolean) => {
     // rebind events after render
     if (!isUnmount.current) {
       if (!chartContext.current || !chartContext.current.chart) {
         return;
       }
 
-      bindEventsToChart(chartContext.current.chart, props, eventsBinded.current, CHART_EVENTS);
+      if (rebindEvent) {
+        bindEventsToChart(chartContext.current.chart, props, eventsBinded.current, CHART_EVENTS);
+      }
 
       const newView = chartContext.current.chart.getCompiler().getVGrammarView();
 
@@ -209,6 +211,9 @@ const BaseChart: React.FC<Props> = React.forwardRef((props, ref) => {
 
   const renderChart = () => {
     if (chartContext.current.chart) {
+      // event should bind before render when the chart has not been rendered
+      bindEventsToChart(chartContext.current.chart, props, eventsBinded.current, CHART_EVENTS);
+
       chartContext.current.chart.renderSync({
         reuse: false
       });
@@ -233,11 +238,11 @@ const BaseChart: React.FC<Props> = React.forwardRef((props, ref) => {
     if (hasSpec) {
       if (!isEqual(eventsBinded.current.spec, props.spec, { skipFunction: skipFunctionDiff })) {
         chartContext.current.chart.updateSpecSync(parseSpec(props), undefined, props.morphConfig ?? defaultMorphConfig);
-        handleChartRender();
+        handleChartRender(true);
         eventsBinded.current = props;
       } else if (eventsBinded.current.data !== props.data) {
         chartContext.current.chart.updateFullDataSync(props.data as any);
-        handleChartRender();
+        handleChartRender(true);
         eventsBinded.current = props;
       }
       return;
@@ -253,7 +258,7 @@ const BaseChart: React.FC<Props> = React.forwardRef((props, ref) => {
       specFromChildren.current = newSpecFromChildren;
 
       chartContext.current.chart.updateSpecSync(parseSpec(props), undefined, props.morphConfig ?? defaultMorphConfig);
-      handleChartRender();
+      handleChartRender(true);
       eventsBinded.current = props;
     }
   }, [props]);
