241f78faa16ed756c6ab8e8c7ce12935cff1f166
xile611 <github-actions[bot]@users.noreply.github.com>
2025-03-24 11:31:17 +0000

    build: prelease version 1.13.8-alpha.4

--- packages/vchart-types/types/component/common/trigger/desktop.d.ts
+++ packages/vchart-types/types/component/common/trigger/desktop.d.ts
@@ -1,10 +1,8 @@
 import type { IZoomTrigger } from './interface';
 export declare class DeskTopTrigger implements IZoomTrigger {
-    pointerId: number;
     parserScrollEvent(e: any): any;
     parserZoomEvent(e: any): any;
     clearZoom(): void;
     clearScroll(): void;
     clearDrag(): void;
-    parserDragEvent(): boolean;
 }
--- packages/vchart-types/types/component/common/trigger/mobile.d.ts
+++ packages/vchart-types/types/component/common/trigger/mobile.d.ts
@@ -1,9 +1,7 @@
 import type { IZoomTrigger } from './interface';
 export declare class MobileTrigger implements IZoomTrigger {
     private _lastScale;
-    pointerId: number;
     clearZoom(): void;
-    parserDragEvent(event: any): boolean;
     parserZoomEvent(event: any): any;
     parserScrollEvent(event: any): any;
     clearScroll(): void;
