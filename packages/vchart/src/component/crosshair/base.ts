068741fd456f66f00d0a6ffd32f3ea01278d1877
xile611 <dingling112@gmail.com>
2025-03-25 11:38:20 +0800

    fix: crosshair should hide when datazoom change

--- packages/vchart/src/component/crosshair/base.ts
+++ packages/vchart/src/component/crosshair/base.ts
@@ -78,7 +78,7 @@ export abstract class BaseCrossHair<T extends ICartesianCrosshairSpec | IPolarCr
   ): void;
 
   abstract setAxisValue(v: StringOrNumber, axis: IAxis): void;
-  abstract layoutByValue(v?: number): void;
+  abstract layoutByValue(enableRemain?: boolean): void;
   protected abstract _getDatumAtPoint(axis: IAxis, point: IPoint): number | string;
 
   /**
@@ -138,7 +138,7 @@ export abstract class BaseCrossHair<T extends ICartesianCrosshairSpec | IPolarCr
       const { axis, value } = d;
       this.setAxisValue(value, axis);
     });
-    this.layoutByValue();
+    this.layoutByValue(false);
   }
 
   protected _getLimitBounds() {
@@ -187,14 +187,14 @@ export abstract class BaseCrossHair<T extends ICartesianCrosshairSpec | IPolarCr
     });
 
     if (hasVisible) {
-      this.layoutByValue();
+      this.layoutByValue(false);
     }
   }
 
   protected _showDefaultCrosshair() {
     if (this.showDefault) {
       this._showDefaultCrosshairBySpec();
-      this.layoutByValue();
+      this.layoutByValue(false);
     } else {
       this._updateVisibleCrosshair();
     }
