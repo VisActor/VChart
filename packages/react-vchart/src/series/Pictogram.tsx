3802ad847cefce6ec5dae97e4f9fa52de48e5cc8
xile611 <dingling112@gmail.com>
2025-05-29 15:49:07 +0800

    fix: upgrade vrender and vtable

--- packages/react-vchart/src/series/Pictogram.tsx
+++ packages/react-vchart/src/series/Pictogram.tsx
@@ -1,7 +1,7 @@
 import type { BaseSeriesProps } from './BaseSeries';
 import { createSeries } from './BaseSeries';
-import type { IPictogramSeriesSpec } from '@visactor/vchart';
-import { registerPictogramSeries } from '@visactor/vchart';
+import type { IPictogramSeriesSpec } from '@visactor/vchart-extension';
+import { registerPictogramSeries } from '@visactor/vchart-extension';
 
 export type PictogramProps = BaseSeriesProps & Omit<IPictogramSeriesSpec, 'type'>;
 
