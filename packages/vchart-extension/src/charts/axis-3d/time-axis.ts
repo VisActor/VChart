39eab365d4be69402cc341721d696484ffd25854
xile611 <dingling112@gmail.com>
2025-06-03 15:53:14 +0800

    fix: fix register of 3d axis

--- packages/vchart-extension/src/charts/axis-3d/time-axis.ts
+++ packages/vchart-extension/src/charts/axis-3d/time-axis.ts
@@ -1,43 +1,19 @@
-import type { ICartesianTimeAxisSpec } from '@visactor/vchart';
-import { LinearAxisMixin, CartesianTimeAxis, Factory, isZAxis } from '@visactor/vchart';
-import { mixin } from '@visactor/vutils';
-import { getUpdateAttributeOfZAxis } from './util';
+import { CartesianTimeAxis, Factory, registerCartesianTimeAxis } from '@visactor/vchart';
 import { axisZ } from './theme';
+import { mixin } from '@visactor/vutils';
+import { Axis3dMixin } from './axis-3d-mixin';
 
-export interface CartesianTimeAxis3d<T extends ICartesianTimeAxisSpec = ICartesianTimeAxisSpec>
-  extends Pick<LinearAxisMixin, 'valueToPosition'>,
-    CartesianTimeAxis<T> {}
+export const registerCartesianTimeAxis3d = () => {
+  registerCartesianTimeAxis();
+  const AxisCls = Factory.getComponentInKey(CartesianTimeAxis.type);
 
-export class CartesianTimeAxis3d<
-  T extends ICartesianTimeAxisSpec = ICartesianTimeAxisSpec
-> extends CartesianTimeAxis<T> {
-  static readonly builtInTheme = {
-    ...CartesianTimeAxis.builtInTheme,
+  (AxisCls as any).builtInTheme = {
+    ...AxisCls.builtInTheme,
     axisZ: {
-      ...CartesianTimeAxis.builtInTheme.axisX,
+      ...AxisCls.builtInTheme.axisX,
       ...axisZ
     }
   };
 
-  layout3dBox?: { width: number; height: number; length: number };
-
-  setLayout3dBox(box3d: { width: number; height: number; length: number }) {
-    this.layout3dBox = box3d;
-  }
-
-  protected _getUpdateAttribute(ignoreGrid: boolean) {
-    const isZ = isZAxis(this._orient);
-
-    if (!isZ) {
-      return super._getUpdateAttribute(ignoreGrid);
-    }
-
-    return getUpdateAttributeOfZAxis(this, ignoreGrid);
-  }
-}
-
-mixin(CartesianTimeAxis3d, LinearAxisMixin);
-
-export const registerCartesianTimeAxis3d = () => {
-  Factory.registerComponent(CartesianTimeAxis.type, CartesianTimeAxis3d, false);
+  mixin(AxisCls, Axis3dMixin);
 };
