cfe096c27dafc5b1d1e1bd8b8062256e4dcaa782
xuefei1313 <github-actions[bot]@users.noreply.github.com>
2025-10-20 12:59:32 +0000

    build: prelease version 1.13.21-alpha.8

--- packages/vchart-types/types/plugin/chart/interface.d.ts
+++ packages/vchart-types/types/plugin/chart/interface.d.ts
@@ -8,6 +8,8 @@ export interface IChartPlugin<T extends IChartPluginService = any> extends IBase
     onAfterChartSpecTransform?: (service: T, chartSpec: any, actionSource: VChartRenderActionSource) => MaybePromise<void>;
     onAfterModelSpecTransform?: (service: T, chartSpec: any, chartSpecInfo: IChartSpecInfo, actionSource: VChartRenderActionSource) => MaybePromise<void>;
     onBeforeInitChart?: (service: T, chartSpec: any, actionSource: VChartRenderActionSource) => MaybePromise<void>;
+    onLayoutRectUpdate?: (service: T) => void;
+    onAfterRender?: (service: T) => void;
 }
 export interface IChartPluginConstructor {
     readonly pluginType: 'chart';
@@ -22,4 +24,6 @@ export interface IChartPluginService<T extends IChartPlugin = any> extends IBase
     onAfterChartSpecTransform?: (chartSpec: any, actionSource: VChartRenderActionSource) => MaybePromise<void>;
     onAfterModelSpecTransform?: (chartSpec: any, chartSpecInfo: IChartSpecInfo, actionSource: VChartRenderActionSource) => MaybePromise<void>;
     onBeforeInitChart?: (chartSpec: any, actionSource: VChartRenderActionSource) => MaybePromise<void>;
+    onLayoutRectUpdate?: () => void;
+    onAfterRender?: () => void;
 }
