da7d747a7ca8c165540466e45c5726a2721624d8
lixuefei.1313 <lixuef1313@163.com>
2026-02-06 17:20:39 +0800

    fix: fix bug in test

--- packages/vchart/__tests__/unit/component/marker/mark-point/mark-point-style.test.ts
+++ packages/vchart/__tests__/unit/component/marker/mark-point/mark-point-style.test.ts
@@ -1,5 +1,5 @@
-import { BaseMarkPoint } from '../../../../../../src/component/marker/mark-point/base-mark-point';
-import { ComponentTypeEnum } from '../../../../../../src/component/interface/type';
+import { BaseMarkPoint } from '../../../../../src/component/marker/mark-point/base-mark-point';
+import { ComponentTypeEnum } from '../../../../../src/component/interface/type';
 
 class TestMarkPoint extends BaseMarkPoint {
   static type = ComponentTypeEnum.markPoint;
@@ -9,13 +9,19 @@ class TestMarkPoint extends BaseMarkPoint {
   _computePointsAttr() {
     return {};
   }
-  public getMarkerComponent() {
+  _computeOptions() {
+    return {} as any;
+  }
+  _markerLayout() {}
+  _initDataView() {}
+  getMarkerComponent() {
     // Initialize internal properties that _createMarkerComponent might need
-    this._markerData = {} as any;
-    this._markAttributeContext = {} as any;
-    this._relativeSeries = {
+    (this as any)._markerData = {} as any;
+    (this as any)._markAttributeContext = {} as any;
+    (this as any)._relativeSeries = {
       getRegion: () => ({ width: 100, height: 100 })
     } as any;
+    // @ts-ignore
     return this._createMarkerComponent();
   }
 }
