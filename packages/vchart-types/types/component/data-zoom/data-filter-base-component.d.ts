99956c5a55e36ba097a420c4df180535b5c3b625
xuefei1313 <xuefei1313@users.noreply.github.com>
2026-05-15 03:45:25 +0000

    build: prerelease version 2.1.0-alpha.4 [skip ci]

--- packages/vchart-types/types/component/data-zoom/data-filter-base-component.d.ts
+++ packages/vchart-types/types/component/data-zoom/data-filter-base-component.d.ts
@@ -76,13 +76,9 @@ export declare abstract class DataFilterBaseComponent<T extends IDataFilterCompo
     created(): void;
     initLayout(): void;
     init(option: IModelInitOption): void;
-    _compareSpec(spec: AdaptiveSpec<T, 'width' | 'height'>, prevSpec: AdaptiveSpec<T, 'width' | 'height'>): {
-        change: boolean;
-        reMake: boolean;
-        reRender: boolean;
-        reSize: boolean;
-        reCompile: boolean;
-    };
+    _compareSpec(spec: AdaptiveSpec<T, 'width' | 'height'>, prevSpec: AdaptiveSpec<T, 'width' | 'height'>): import("../../model/interface").IUpdateSpecResult;
+    protected _getComponentOnlySpecKeys(): Record<string, boolean>;
+    protected _isComponentOnlySpecChange(spec: AdaptiveSpec<T, 'width' | 'height'>, prevSpec: AdaptiveSpec<T, 'width' | 'height'>): boolean;
     reInit(spec?: AdaptiveSpec<T, 'width' | 'height'>): void;
     onLayoutStart(layoutRect: IRect, viewRect: ILayoutRect): void;
     updateLayoutAttribute(): void;
