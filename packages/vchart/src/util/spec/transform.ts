c65006479c4915876195a5dc0286a1b4cac31311
skie1997 <skie1997@outlook.com>
2026-07-14 17:12:54 +0800

    feat: theme support callback function. close#4620

--- packages/vchart/src/util/spec/transform.ts
+++ packages/vchart/src/util/spec/transform.ts
@@ -1,5 +1,8 @@
 import { isArray, isFunction, isPlainObject, isString } from '@visactor/vutils';
-import type { ISeriesSpec } from '../../typings';
+
+interface IFunctionTransformRegistry {
+  getFunction: (key: string) => unknown;
+}
 
 // todo 以目前的场景来看，并没有递归的需要。
 // 考虑到不确定性，还是递归处理spec对象，时间消耗很少
@@ -38,33 +41,31 @@ export function specTransform(
 
 /**
  * functionTransform is used to replace the function registered by the instance
- * @param spec
+ * @param value
  * @returns
  */
-export function functionTransform(spec: ISeriesSpec, VChart: any): any {
-  if (!spec) {
-    return spec;
+export function functionTransform(value: unknown, registry: IFunctionTransformRegistry): any {
+  if (!value) {
+    return value;
+  }
+  if (isString(value)) {
+    return registry.getFunction(value) ?? value;
   }
   // 如果是普通对象
-  if (isPlainObject(spec)) {
+  if (isPlainObject(value)) {
     const result: any = {};
-    for (const key in spec as any) {
-      if (Object.prototype.hasOwnProperty.call(spec, key)) {
-        // 如果使用了注册函数
-        if (isString(spec[key]) && VChart.getFunction(spec[key])) {
-          result[key] = VChart.getFunction(spec[key]);
-          continue;
-        }
-        result[key] = functionTransform(spec[key], VChart);
+    for (const key in value as any) {
+      if (Object.prototype.hasOwnProperty.call(value, key)) {
+        result[key] = functionTransform((value as any)[key], registry);
       }
     }
     return result;
   }
   // 如果是数组
-  if (isArray(spec)) {
-    return (spec as ISeriesSpec[]).map((s: ISeriesSpec) => functionTransform(s, VChart));
+  if (isArray(value)) {
+    return value.map(item => functionTransform(item, registry));
   }
-  return spec;
+  return value;
 }
 
 export function transformFunctionAttribute(att: unknown, ...args: unknown[]) {
