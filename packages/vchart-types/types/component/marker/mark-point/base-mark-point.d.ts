857a48ceaa738b472fde21a63dbb9d43eca3ed1e
lixuefei.1313 <lixuef1313@163.com>
2026-07-16 16:40:50 +0800

    feat: support dimension hover state for mark point target

--- packages/vchart-types/types/component/marker/mark-point/base-mark-point.d.ts
+++ packages/vchart-types/types/component/marker/mark-point/base-mark-point.d.ts
@@ -1,6 +1,7 @@
 import type { IMarkPoint, IMarkPointSpec } from './interface';
 import { MarkPoint as MarkPointComponent } from '@visactor/vrender-components';
 import { BaseMarker } from '../base-marker';
+import type { DimensionEventParams } from '../../../event/events/dimension/interface';
 import type { IGroup } from '@visactor/vrender-core';
 export declare abstract class BaseMarkPoint extends BaseMarker<IMarkPointSpec> implements IMarkPoint {
     static specKey: string;
@@ -8,6 +9,10 @@ export declare abstract class BaseMarkPoint extends BaseMarker<IMarkPointSpec> i
     layoutZIndex: number;
     protected _markerComponent: MarkPointComponent;
     protected abstract _computePointsAttr(): any;
+    protected initEvent(): void;
+    protected _handleDimensionHover: (params: DimensionEventParams) => void;
+    private _getTargetSymbolGraphic;
+    private _isDimensionHoverTarget;
     static _getMarkerCoordinateType(markerSpec: any): string;
     protected _createMarkerComponent(): IGroup;
     protected _markerLayout(): void;
