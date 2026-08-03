84d8ac611aacf9ee4108655871b1e5fae1eed22a
xuefei1313 <xuefei1313@users.noreply.github.com>
2026-08-03 13:43:43 +0000

    build: prerelease version 2.1.6-alpha.1 [skip ci]

--- packages/vchart-types/types/component/marker/mark-point/base-mark-point.d.ts
+++ packages/vchart-types/types/component/marker/mark-point/base-mark-point.d.ts
@@ -1,7 +1,7 @@
 import type { IMarkPoint, IMarkPointSpec } from './interface';
 import { MarkPoint as MarkPointComponent } from '@visactor/vrender-components';
 import { BaseMarker } from '../base-marker';
-import type { DimensionEventParams } from '../../../event/events/dimension/interface';
+import { type DimensionEventParams } from '../../../event/events/dimension/interface';
 import type { IGroup } from '@visactor/vrender-core';
 export declare abstract class BaseMarkPoint extends BaseMarker<IMarkPointSpec> implements IMarkPoint {
     static specKey: string;
